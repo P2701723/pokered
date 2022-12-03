f936194c32bccdb3dbde8dcfd14655ba369cdec9
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 18:08:20 -0500

    Update route_2.asm

--- text/maps/route_2.asm
+++ text/maps/route_2.asm
@@ -1,7 +1,8 @@
 _Route2Text3:: ; 8d745 (23:5745)
 	text "ROUTE 2"
-	line "VIRIDIAN CITY -"
-	cont "PEWTER CITY"
+	
+	para "VIRIDIAN CITY -"
+	line "PEWTER CITY"
 	done
 
 _Route2Text4:: ; 8d76a (23:576a)
