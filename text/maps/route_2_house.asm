bd1a6df77be91a95a68a4eec7e5a8780a8dd7f93
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 18:09:26 -0500

    Update route_2_house.asm

--- text/maps/route_2_house.asm
+++ text/maps/route_2_house.asm
@@ -1,7 +1,8 @@
 _Route2HouseText1:: ; 8a7b8 (22:67b8)
 	text "A fainted #MON"
-	line "can't fight. But, "
-	cont "it can still use "
-	cont "moves like CUT!"
-	done
+	line "can't fight."
+	
+	para "But, it can still"
+	line "use HM moves like"
+	cont "FLASH and CUT!"
 
