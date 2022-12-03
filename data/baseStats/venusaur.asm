d4efa7381f13dd60f87d3aa7a662e7c26fb70e8f
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 13:31:14 -0500

    Update venusaur.asm

--- data/baseStats/venusaur.asm
+++ data/baseStats/venusaur.asm
@@ -1,10 +1,10 @@
 VenusaurBaseStats: ; 38416 (e:4416)
 db DEX_VENUSAUR ; pokedex id
 db 80 ; base hp
-db 82 ; base attack
-db 83 ; base defense
+db 92 ; base attack   +10
+db 103 ; base defense +20
 db 80 ; base speed
-db 100 ; base special
+db 110 ; base special +10
 db GRASS ; species type 1
 db POISON ; species type 2
 db 45 ; catch rate
@@ -15,15 +15,15 @@ dw VenusaurPicBack
 ; attacks known at lvl 0
 db TACKLE
 db GROWL
-db LEECH_SEED
 db VINE_WHIP
+db LEECH_SEED
 db 3 ; growth rate
 ; learnset
 db %10100100
 db %01000011
 db %00111000
-db %11000000
-db %00000011
+db %11000110 ; Earthquake, Fissure
+db %10000011 ; Skull Bash
 db %00001000
-db %00000110
+db %01100110 ; Strength, Flash
 db 0 ; padding
