3ed28feb3c83c8910539c6348604aff302e568a0
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 17:43:56 -0500

    Update primeape.asm

--- data/baseStats/primeape.asm
+++ data/baseStats/primeape.asm
@@ -1,10 +1,10 @@
 PrimeapeBaseStats: ; 389fe (e:49fe)
 db DEX_PRIMEAPE ; pokedex id
-db 65 ; base hp
-db 105 ; base attack
-db 60 ; base defense
-db 95 ; base speed
-db 60 ; base special
+db 75 ; base hp		+10
+db 115 ; base attack	+10
+db 70 ; base defense	+10
+db 105 ; base speed	+10
+db 80 ; base special	+20
 db FIGHTING ; species type 1
 db FIGHTING ; species type 2
 db 75 ; catch rate
@@ -14,20 +14,15 @@ dw PrimeapePicFront
 dw PrimeapePicBack
 ; attacks known at lvl 0
 db SCRATCH
-IF !DEF(_YELLOW)
-	db LEER
-ENDC
 db KARATE_CHOP
 db FURY_SWIPES
-IF DEF(_YELLOW)
-	db LOW_KICK
-ENDC
+db LOW_KICK
 db 0 ; growth rate
 ; learnset
 db %10110001
 db %11000011
 db %10001111
-db %11001001
+db %11001111 ; Earthquake, Fissure
 db %11000110
 db %10001000
 db %00100010
