229a789676f55830c2fa3e8351ef992053f7f26f
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 16:58:58 -0500

    Update nidoking.asm

--- data/baseStats/nidoking.asm
+++ data/baseStats/nidoking.asm
@@ -1,10 +1,10 @@
 NidokingBaseStats: ; 3877a (e:477a)
 db DEX_NIDOKING ; pokedex id
-db 81 ; base hp
-db 92 ; base attack
-db 77 ; base defense
-db 85 ; base speed
-db $69 ; base special
+db 91 ; base hp       +10
+db 112 ; base attack  +20
+db 87 ; base defense  +10
+db 95 ; base speed    +10
+db 95 ; base special  +20
 db POISON ; species type 1
 db GROUND ; species type 2
 db 45 ; catch rate
@@ -13,17 +13,17 @@ INCBIN "pic/bmon/nidoking.pic",0,1 ; 77, sprite dimensions
 dw NidokingPicFront
 dw NidokingPicBack
 ; attacks known at lvl 0
-db TACKLE
 db HORN_ATTACK
 db POISON_STING
-db THRASH
+db FOCUS_ENERGY
+db DOUBLE_KICK
 db 3 ; growth rate
 ; learnset
 db %11110001
 db %11111111
 db %10001111
-db %11000111
-db %10100011
+db %11001111 ; Dig
+db %10110011 ; Flamethrower
 db %10001000
-db %00110010
+db %00110110 ; Cut
 db 0 ; padding
