b201d981df5435462472e7bf71c6cf04e02c94ea
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 14:38:47 -0500

    Update route1.asm

--- data/wildPokemon/route1.asm
+++ data/wildPokemon/route1.asm
@@ -1,14 +1,14 @@
 Route1Mons:
 	db $19
-	db 4,SPEAROW
-	db 4,MEOWTH
-	db 3,DODUO
+	db 3,PIDGEY	; 50% Pidgey L2-7
+	db 3,RATTATA	; 40% Rattata L2-4
+	db 2,RATTATA	; 10% Spearow L3
+	db 2,PIDGEY
+	db 3,SPEAROW
 	db 4,PIDGEY
-	db 3,PIKACHU
-	db 4,ODDISH
-	db 4,BELLSPROUT
-	db 3,CUBONE
-	db 3,TANGELA
-	db 4,EXEGGCUTE
+	db 4,RATTATA
+	db 5,PIDGEY
+	db 6,PIDGEY
+	db 7,PIDGEY
 	db $00
 
