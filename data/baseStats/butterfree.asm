b00ee94a82a88afbefba5a8861971eb477196e24
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 18:34:42 -0500

    Update butterfree.asm

--- data/baseStats/butterfree.asm
+++ data/baseStats/butterfree.asm
@@ -1,10 +1,10 @@
 ButterfreeBaseStats: ; 38512 (e:4512)
 db DEX_BUTTERFREE ; pokedex id
-db 60 ; base hp
-db 45 ; base attack
-db 50 ; base defense
-db 70 ; base speed
-db 80 ; base special
+db 80 ; base hp		+20
+db 70 ; base attack	+25
+db 70 ; base defense	+20
+db 100 ; base speed	+30
+db 90 ; base special	+10
 db BUG ; species type 1
 db FLYING ; species type 2
 db 45 ; catch rate
@@ -13,10 +13,10 @@ INCBIN "pic/bmon/butterfree.pic",0,1 ; 77, sprite dimensions
 dw ButterfreePicFront
 dw ButterfreePicBack
 ; attacks known at lvl 0
+db HARDEN
+db STRING_SHOT
+db TACKLE
 db CONFUSION
-db 0
-db 0
-db 0
 db 0 ; growth rate
 ; learnset
 db %00101010
@@ -24,10 +24,6 @@ db %01000011
 db %00111000
 db %11110000
 db %01000011
-db %00101000
-IF DEF(_YELLOW)
-	db %01000010
-ELSE
-	db %00000010
-ENDC
+db %00101010 ; Dream Eater
+db %01000010 ; Flash
 db 0 ; padding
