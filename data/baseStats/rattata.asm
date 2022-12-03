38a4e2cc6e4f14a61e595550e026b5f256b1aa67
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 15:11:09 -0500

    Update rattata.asm

--- data/baseStats/rattata.asm
+++ data/baseStats/rattata.asm
@@ -1,10 +1,10 @@
 RattataBaseStats: ; 385d6 (e:45d6)
 db DEX_RATTATA ; pokedex id
-db 30 ; base hp
+db 40 ; base hp       +10
 db 56 ; base attack
 db 35 ; base defense
 db 72 ; base speed
-db 25 ; base special
+db 35 ; base special  +10
 db NORMAL ; species type 1
 db NORMAL ; species type 2
 db 255 ; catch rate
@@ -20,10 +20,10 @@ db 0
 db 0 ; growth rate
 ; learnset
 db %10100000
-db %00101111
-db %10001000
+db %00111111 ; Ice Beam
+db %10001010 ; Counter
 db %11001001
 db %11000010
-db %00001000
-db %00000010
+db %00011000 ; Thunder Wave
+db %00000110 ; Cut
 db 0 ; padding
