c90b79d4c14382dd056b6b6e6c12109a13f381cd
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 18:59:24 -0500

    Update beedrill.asm

--- data/baseStats/beedrill.asm
+++ data/baseStats/beedrill.asm
@@ -1,10 +1,10 @@
 BeedrillBaseStats: ; 38566 (e:4566)
 db DEX_BEEDRILL ; pokedex id
-db $69 ; base hp
-db $64 ; base attack
-db $50 ; base defense
-db $55 ; base speed
-db $5f ; base special
+db 90 ; base hp       +25
+db 120 ; base attack  +40
+db 50 ; base defense  +10
+db 110 ; base speed   +35
+db 65 ; base special  +20
 db BUG ; species type 1
 db POISON ; species type 2
 db 45 ; catch rate
@@ -13,17 +13,17 @@ INCBIN "pic/bmon/beedrill.pic",0,1 ; 77, sprite dimensions
 dw BeedrillPicFront
 dw BeedrillPicBack
 ; attacks known at lvl 0
+db HARDEN
+db STRING_SHOT
+db POISON_STING
 db FURY_ATTACK
-db 0
-db 0
-db 0
 db 0 ; growth rate
 ; learnset
 db %00100100
 db %01000011
-db %00011000
+db %00111000 ; Solarbeam
 db %11000000
 db %11000011
 db %00001000
-db %00000110
+db %01000110 ; Flash
 db 0 ; padding
