eaac3f010296104f704def359f54ecd80746733b
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 13:43:45 -0500

    Update charmander.asm

--- data/baseStats/charmander.asm
+++ data/baseStats/charmander.asm
@@ -1,10 +1,10 @@
 CharmanderBaseStats: ; 38432 (e:4432)
 db DEX_CHARMANDER ; pokedex id
 db 39 ; base hp
-db 52 ; base attack
-db 43 ; base defense
+db 56 ; base attack   +4
+db 45 ; base defense  +2
 db 65 ; base speed
-db 50 ; base special
+db 60 ; base special  +10
 db FIRE ; species type 1
 db FIRE ; species type 2
 db 45 ; catch rate
@@ -23,7 +23,7 @@ db %10110101
 db %00000011
 db %01001111
 db %11001000
-db %11100011
-db %00001000
+db %11110011 ; Flamethrower
+db %10001000 ; Rock Slide
 db %00100110
 db 0 ; padding
