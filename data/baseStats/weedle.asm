a58bc7829f0917ec834b1e0667acf8bb788beb08
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 18:36:02 -0500

    Update weedle.asm

--- data/baseStats/weedle.asm
+++ data/baseStats/weedle.asm
@@ -1,10 +1,10 @@
 WeedleBaseStats: ; 3852e (e:452e)
 db DEX_WEEDLE ; pokedex id
 db 40 ; base hp
-db 35 ; base attack
+db 50 ; base attack   +15
 db 30 ; base defense
-db 50 ; base speed
-db 20 ; base special
+db 60 ; base speed    +10
+db 30 ; base special  +10
 db BUG ; species type 1
 db POISON ; species type 2
 db 255 ; catch rate
@@ -19,11 +19,11 @@ db 0
 db 0
 db 0 ; growth rate
 ; learnset
-db %00000000
-db %00000000
-db %00000000
-db %00000000
-db %00000000
-db %00000000
-db %00000000
+db %00100000 ; Toxic
+db %00000011 ; Headbutt, Double-Edge
+db %00001000 ; Rage
+db %11000000 ; Mimic, Double Team
+db %00000010 ; Bide
+db %00001000 ; Rest
+db %00000010 ; Substitute
 db 0 ; padding
