d40615b4682a12a4a015c217c4b699031b058025
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 16:10:25 -0500

    Update route22.asm

--- data/wildPokemon/route22.asm
+++ data/wildPokemon/route22.asm
@@ -1,14 +1,14 @@
 Route22Mons:
 	db $19
-	db 5,ODDISH
-	db 5,NIDORAN_M
-	db 5,EKANS
-	db 5,KOFFING
-	db 5,GRIMER
-	db 5,VULPIX
-	db 5,SPEAROW
-	db 5,DODUO
-	db 5,PIKACHU
-	db 5,NIDORAN_F
+	db 4,RATTATA	; 30% Rattata L4
+	db 4,NIDORAN_F	; 25% Nidoran-M L4
+	db 4,NIDORAN_M	; 25% Nidoran-F L4
+	db 4,RATTATA	; 10% Spearow L5
+	db 5,SPEAROW	; 9% Mankey L5
+	db 4,NIDORAN_M	; 1% Poliwag L5
+	db 4,NIDORAN_F
+	db 5,MANKEY
+	db 5,MANKEY
+	db 5,POLIWAG
 	db $00
 
