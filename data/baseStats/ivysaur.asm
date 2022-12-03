56a54204f9a4bbe46e2101a3f28994cccfe717dd
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 13:30:57 -0500

    Update ivysaur.asm

--- data/baseStats/ivysaur.asm
+++ data/baseStats/ivysaur.asm
@@ -1,10 +1,10 @@
 IvysaurBaseStats: ; 383fa (e:43fa)
 db DEX_IVYSAUR ; pokedex id
 db 60 ; base hp
-db 62 ; base attack
-db 63 ; base defense
+db 67 ; base attack   +5
+db 73 ; base defense  +10
 db 60 ; base speed
-db 80 ; base special
+db 85 ; base special  +5
 db GRASS ; species type 1
 db POISON ; species type 2
 db 45 ; catch rate
@@ -15,7 +15,7 @@ dw IvysaurPicBack
 ; attacks known at lvl 0
 db TACKLE
 db GROWL
-db LEECH_SEED
+db VINE_WHIP
 db 0
 db 3 ; growth rate
 ; learnset
@@ -23,7 +23,7 @@ db %10100100
 db %00000011
 db %00111000
 db %11000000
-db %00000011
+db %10000011 ; Skull Bash
 db %00001000
-db %00000110
+db %01100110 ; Strength, Flash
 db 0 ; padding
