622315529d60c5b26ff32a2d334f227809d4493a
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 18:20:53 -0500

    Update route2.asm

--- data/wildPokemon/route2.asm
+++ data/wildPokemon/route2.asm
@@ -1,14 +1,14 @@
 Route2Mons:
 	db $19
-	db 5,POLIWAG
-	db 6,JIGGLYPUFF
-	db 5,EKANS
-	db 6,NIDORAN_F
-	db 5,NIDORAN_M
-	db 5,GRIMER
-	db 6,RATTATA
-	db 5,CHARMANDER
-	db 5,SQUIRTLE
+	db 5,RATTATA	; 35% Rattata L5
+	db 5,PIDGEY	; 24% Pidgey L5-7
+	db 5,RATTATA	; 10% Spearow L5
+	db 5,SPEAROW	; 10% Nidoran-M L4
+	db 4,NIDORAN_M	; 10% Nidoran-F L4
+	db 4,NIDORAN_F	; 5% Caterpie L3
+	db 3,CATERPIE	; 5% Weedle L3
+	db 3,WEEDLE	; 1% Bulbasaur L5
+	db 7,PIDGEY
 	db 5,BULBASAUR
 	db $00
 
