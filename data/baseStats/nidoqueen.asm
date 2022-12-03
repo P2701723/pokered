dcb838e33606d25f43603f371ec72dcaa3f264a9
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 16:59:11 -0500

    Update nidoqueen.asm

--- data/baseStats/nidoqueen.asm
+++ data/baseStats/nidoqueen.asm
@@ -1,10 +1,10 @@
 NidoqueenBaseStats: ; 38726 (e:4726)
 db DEX_NIDOQUEEN ; pokedex id
-db 90 ; base hp
-db $5c ; base attack
-db $6b ; base defense
-db 76 ; base speed
-db 75 ; base special
+db 100 ; base hp      +10
+db 102 ; base attack  +20
+db 97 ; base defense  +10
+db 86 ; base speed    +10
+db 95 ; base special  +20
 db POISON ; species type 1
 db GROUND ; species type 2
 db 45 ; catch rate
@@ -13,17 +13,17 @@ INCBIN "pic/bmon/nidoqueen.pic",0,1 ; 77, sprite dimensions
 dw NidoqueenPicFront
 dw NidoqueenPicBack
 ; attacks known at lvl 0
-db TACKLE
-db SCRATCH
+db BITE
+db POISON_STING
 db TAIL_WHIP
-db BODY_SLAM
+db DOUBLE_KICK
 db 3 ; growth rate
 ; learnset
 db %11110001
 db %11111111
 db %10001111
-db %11000111
-db %10100011
+db %11001111 ; Dig
+db %10110011 ; Flamethrower
 db %10001000
-db %00110010
+db %00110110 ; Cut
 db 0 ; padding
