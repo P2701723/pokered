69b307bee22a4168c2069f7c1d31b964360d63ba
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 19:13:51 -0500

    Update viridianforest.asm

--- data/wildPokemon/viridianforest.asm
+++ data/wildPokemon/viridianforest.asm
@@ -1,14 +1,14 @@
 ForestMons:
 	db $08
-	db 5,VENONAT
-	db 5,CATERPIE
-	db 5,WEEDLE
-	db 5,DIGLETT
-	db 4,ABRA
-	db 6,KAKUNA
-	db 6,METAPOD
-	db 5,VULPIX
-	db 5,PSYDUCK
+	db 5,CATERPIE	; 30% Caterpie
+	db 5,WEEDLE	; 20% Weedle
+	db 4,METAPOD	; 15% Metapod
+	db 5,CATERPIE	; 10% Kakuna
+	db 4,KAKUNA	; 10% Pidgey
+	db 6,PIDGEY	; 10% Pikachu
+	db 5,PIKACHU	; 4% Pidgeotto
+	db 5,PIKACHU	; 1% Bulbasaur
+	db 9,PIDGEOTTO
 	db 5,BULBASAUR
 	db $00
 
