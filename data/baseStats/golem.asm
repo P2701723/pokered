d3c726da7f25127fbcd0460f0405b5734e9a1621
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 21:33:26 -0500

    Update golem.asm

--- data/baseStats/golem.asm
+++ data/baseStats/golem.asm
@@ -1,10 +1,10 @@
 GolemBaseStats: ; 38c12 (e:4c12)
 db DEX_GOLEM ; pokedex id
 db 80 ; base hp
-db 110 ; base attack
-db 130 ; base defense
-db 45 ; base speed
-db 55 ; base special
+db 120 ; base attack  +10
+db 140 ; base defense +10
+db 60 ; base speed    +15
+db 75 ; base special  +20
 db ROCK ; species type 1
 db GROUND ; species type 2
 db 45 ; catch rate
@@ -23,7 +23,7 @@ db %10110001
 db %01000011
 db %00001111
 db %11001110
-db %00101110
+db %00111110 ; Flamethrower
 db %11001000
 db %00100010
 db 0 ; padding
