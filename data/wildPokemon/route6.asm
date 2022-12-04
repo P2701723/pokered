2dad8c543d3c8bfce99d086a8d555ec8abb72587
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:15:38 -0500

    Update route6.asm

--- data/wildPokemon/route6.asm
+++ data/wildPokemon/route6.asm
@@ -1,14 +1,14 @@
 Route6Mons:
 	db $0f
-	db 20,VENONAT
-	db 20,PIDGEOTTO
-	db 20,FEAROW
-	db 20,POLIWHIRL
-	db 20,GROWLITHE
-	db 20,PONYTA
-	db 20,PARAS
-	db 20,WEEPINBELL
-	db 20,KRABBY
-	db 15,PIDGEY
+	db 17,PIDGEY		; 20% Pidgey
+	db 17,RATTATA		; 20% Rattata
+	db 16,ODDISH		; 15% Oddish
+	db 16,MANKEY		; 15% Bellsprout
+	db 16,MEOWTH		; 10% Mankey
+	db 16,BELLSPROUT	; 10% Meowth
+	db 16,BELLSPROUT	; 5% Abra
+	db 13,ABRA		; 4% Jigglypuff
+	db 13,JIGGLYPUFF	; 1% Pidgeotto
+	db 18,PIDGEOTTO
 	db $00
 
