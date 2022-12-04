6ab0d4dc65da2543ae4af44127f0c690fdfd84f6
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:02:45 -0500

    Update lastisland.asm

--- data/wildPokemon/lastisland.asm
+++ data/wildPokemon/lastisland.asm
@@ -1,13 +1,13 @@
 LastIslandMons:
 	db $0C
-	db 80,GOLBAT
-	db 82,RATICATE
-	db 88,WIGGLYTUFF
-	db 80,ELECTABUZZ
-	db 87,ARBOK
-	db 88,VICTREEBEL
-	db 88,GENGAR
-	db 89,PRIMEAPE
-	db 89,ALAKAZAM
+	db 80,DITTO
+	db 80,DITTO
 	db 90,DITTO
+	db 85,DITTO
+	db 85,DITTO
+	db 85,DITTO
+	db 90,DITTO
+	db 95,DITTO
+	db 95,DITTO
+	db 99,DITTO
 	db $00
