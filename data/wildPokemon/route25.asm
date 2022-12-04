bb8b3a90bb056297fe74ede3bf503ffef13fcf1e
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:30:38 -0500

    Update route25.asm

--- data/wildPokemon/route25.asm
+++ data/wildPokemon/route25.asm
@@ -1,14 +1,14 @@
 Route25Mons:
 	db $0F
-	db 20,SLOWBRO
-	db 20,MANKEY
-	db 20,DROWZEE
-	db 20,BELLSPROUT
-	db 20,VENONAT
-	db 20,CUBONE
-	db 20,EXEGGCUTE
-	db 20,ELECTABUZZ
-	db 20,MAGMAR
-	db 20,SCYTHER
+	db 16,ODDISH		; 20% Pidgey
+	db 16,BELLSPROUT	; 20% Oddish
+	db 16,PIDGEY		; 20% Bellsprout
+	db 14,METAPOD		; 10% Metapod
+	db 14,KAKUNA		; 10% Kakuna
+	db 15,ABRA		; 10% Abra
+	db 16,VENONAT		; 5% Venonat
+	db 17,PIDGEY		; 4% Pidgeotto
+	db 18,PIDGEOTTO		; 1% Squirtle
+	db 15,SQUIRTLE
 	db $00
 
