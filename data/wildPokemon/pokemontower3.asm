ba33c1229c89fc68fb48772fa46d48a87f8bfab8
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:08:04 -0500

    Update pokemontower3.asm

--- data/wildPokemon/pokemontower3.asm
+++ data/wildPokemon/pokemontower3.asm
@@ -1,14 +1,13 @@
 TowerMons3:
 	db $0A
-	db 50,GASTLY
-	db 50,HAUNTER
-	db 50,GASTLY
-	db 50,HAUNTER
-	db 50,GASTLY
-	db 50,HAUNTER
-	db 50,GASTLY
-	db 50,CUBONE
-	db 50,MAROWAK
-	db 50,HAUNTER
+	db 28,GASTLY	; 85% Gastly L28-34
+	db 29,GASTLY	; 10% Cubone L35
+	db 30,GASTLY	; 5% Haunter L37
+	db 31,GASTLY
+	db 32,GASTLY
+	db 35,CUBONE
+	db 33,GASTLY
+	db 34,GASTLY
+	db 37,HAUNTER
+	db 37,HAUNTER
 	db $00
-
