a8b953d953665c58ca1b3e823635c9be2db0426d
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 00:21:56 -0500

    Update prizes.asm

--- data/prizes.asm
+++ data/prizes.asm
@@ -12,53 +12,34 @@ NoThanksText: ; 5284f (14:684f)
 	db "NO THANKS@"
 
 PrizeMenuMon1Entries: ; 52859 (14:6859)
-	db ABRA
-	db STARYU
-	db LICKITUNG
+	db BULBASAUR
+	db CHARMANDER
+	db SQUIRTLE
 	db "@"
 PrizeMenuMon1Cost: ; 5285d (14:685d)
-IF DEF(_RED)
-	db $01,$80
-	db $05,$00
-ENDC
-IF DEF(_BLUE)
-	db $01,$20
-	db $07,$50
-ENDC
-	db $12,$00
+	db $20,$00
+	db $20,$00
+	db $20,$00
 	db "@"
 
 PrizeMenuMon2Entries: ; 52864 (14:6864)
-IF DEF(_RED)
+	db EEVEE
 	db DRATINI
-	db SCYTHER
-ENDC
-IF DEF(_BLUE)
-	db PINSIR
-	db DRATINI
-ENDC
 	db PORYGON
 	db "@"
 PrizeMenuMon2Cost: ; 52868 (14:6868)
-IF DEF(_RED)
-	db $28,$00
-	db $55,$00
+	db $30,$00
+	db $40,$00
 	db $99,$99
-ENDC
-IF DEF(_BLUE)
-	db $25,$00
-	db $46,$00
-	db $65,$00
-ENDC
 	db "@"
 
 PrizeMenuTMsEntries: ; 5286f (14:686f)
-	db TM_23
-	db TM_15
-	db TM_50
+	db MOON_STONE
+	db RARE_CANDY
+	db PP_UP
 	db "@"
 PrizeMenuTMsCost: ; 52873 (14:6873)
-	db $33,$00 ; 3300 Coins
-	db $55,$00 ; 5500 Coins
-	db $77,$00 ; 7700 Coins
+	db $01,$05 ; 105 Coins
+	db $02,$40 ; 240 Coins
+	db $04,$90 ; 490 Coins
 	db "@"
