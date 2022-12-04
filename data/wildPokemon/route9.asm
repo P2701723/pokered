e4e10b796e10be92381e478ca04db4321ba90078
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:16:55 -0500

    Update route9.asm

--- data/wildPokemon/route9.asm
+++ data/wildPokemon/route9.asm
@@ -1,14 +1,14 @@
 Route9Mons:
 	db $0f
-	db 33,CUBONE
-	db 33,PARAS
-	db 33,MAGIKARP
-	db 33,VOLTORB
-	db 33,MACHOP
-	db 33,MEOWTH
-	db 33,KOFFING
-	db 33,PINSIR
-	db 33,WEEPINBELL
-	db 33,DRAGONAIR
+	db 22,EKANS	; 20% Ekans
+	db 22,SANDSHREW	; 20% Sandshrew
+	db 23,RATTATA	; 15% Rattata
+	db 23,SPEAROW	; 10% Spearow
+	db 24,NIDORAN_M	; 10% Nidoran-M
+	db 24,NIDORAN_F	; 10% Nidoran-F
+	db 25,NIDORINO	; 5% Nidorino
+	db 25,NIDORINA	; 5% Nidorina
+	db 26,RATICATE	; 4% Raticate
+	db 26,FEAROW	; 1% Fearow
 	db $00
 
