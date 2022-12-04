28a250865fcbffe8414c9ab1c49089f13ed0b245
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:13:10 -0500

    Update route10.asm

--- data/wildPokemon/route10.asm
+++ data/wildPokemon/route10.asm
@@ -1,14 +1,14 @@
 Route10Mons:
 	db $0F
-	db 35,VOLTORB
-	db 35,ELECTABUZZ
-	db 35,MAGNEMITE
-	db 35,PIKACHU
-	db 35,SANDSLASH
-	db 35,NIDORINA
-	db 35,RHYHORN
-	db 35,VULPIX
-	db 35,GOLBAT
-	db 35,JOLTEON
+	db 25,VOLTORB	; 20% Voltorb
+	db 24,SPEAROW	; 20% Spearow
+	db 25,MAGNEMITE	; 15% Magnemite
+	db 23,EKANS	; 10% Ekans
+	db 23,SANDSHREW	; 10% Sandshrew
+	db 24,RATTATA	; 10% Rattata
+	db 26,NIDORAN_M	; 5% Nidoran-M
+	db 26,NIDORAN_F	; 5% Nidoran-F
+	db 25,MACHOP	; 4% Machop
+	db 27,RATICATE	; 1% Raticate
 	db $00
 
