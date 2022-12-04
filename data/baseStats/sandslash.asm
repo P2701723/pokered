789453d6181878a3d1545234d03271fbd8099d9a
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 21:24:34 -0500

    Update sandslash.asm

--- data/baseStats/sandslash.asm
+++ data/baseStats/sandslash.asm
@@ -2,9 +2,9 @@ SandslashBaseStats: ; 386d2 (e:46d2)
 db DEX_SANDSLASH ; pokedex id
 db 75 ; base hp
 db 100 ; base attack
-db 110 ; base defense
-db 65 ; base speed
-db 55 ; base special
+db 120 ; base defense +10
+db 90 ; base speed    +25
+db 65 ; base special  +10
 db GROUND ; species type 1
 db GROUND ; species type 2
 db 90 ; catch rate
@@ -14,14 +14,14 @@ dw SandslashPicFront
 dw SandslashPicBack
 ; attacks known at lvl 0
 db SCRATCH
+db DEFENSE_CURL
 db SAND_ATTACK
-db 0
 db 0
 db 0 ; growth rate
 ; learnset
 db %10100100
 db %01000011
-db %00001101
+db %00001111 ; Counter
 db %11001110
 db %11000010
 db %10001000
