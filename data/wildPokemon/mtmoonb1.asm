32f098a82c3932fc4e1f98bcc872e85858a1f15e
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:06:54 -0500

    Update mtmoonb1.asm

--- data/wildPokemon/mtmoonb1.asm
+++ data/wildPokemon/mtmoonb1.asm
@@ -1,14 +1,14 @@
 MoonMonsB1:
 	db $0a
-	db 18,DIGLETT
-	db 18,GRIMER
-	db 18,KOFFING
-	db 18,EKANS
-	db 18,VENONAT
-	db 18,ODDISH
-	db 18,BELLSPROUT
-	db 18,JIGGLYPUFF
-	db 18,CLEFAIRY
-	db 18,SLOWPOKE
+	db 12,ZUBAT	; 55% Zubat L12-14
+	db 13,GEODUDE	; 30% Geodude L13-14
+	db 13,ZUBAT	; 10% Paras L13-14
+	db 14,GEODUDE	; 5% Clefairy L14-15
+	db 12,ZUBAT
+	db 14,ZUBAT
+	db 13,PARAS
+	db 14,PARAS
+	db 14,CLEFAIRY
+	db 15,CLEFAIRY
 	db $00
 
