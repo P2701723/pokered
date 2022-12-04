bac7ab97d28dcad453b024db637c91383b0f0a19
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 15:40:17 -0500

    Update copycats_house_1f.asm

--- text/maps/copycats_house_1f.asm
+++ text/maps/copycats_house_1f.asm
@@ -1,8 +1,9 @@
 _CopycatsHouse1FText1:: ; a14f7 (28:54f7)
 	text "My daughter is so"
 	line "self-centered."
-	cont "She only has a"
-	cont "few friends."
+	
+	para "She only has a"
+	line "few friends."
 	done
 
 _CopycatsHouse1FText2:: ; a1535 (28:5535)
@@ -16,10 +17,4 @@ _CopycatsHouse1FText2:: ; a1535 (28:5535)
 	done
 
 _CopycatsHouse1FText3:: ; a1596 (28:5596)
-IF DEF(_YELLOW)
-	text "CHANSEY: Chaan"
-	line "sey!@@"
-ELSE
-	text "CHANSEY: Chaan!"
-	line "Sii!@@"
-ENDC
+	text "Chaan sey!@@"
