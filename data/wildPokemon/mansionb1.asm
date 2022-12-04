778db8d35f789beaa4b1e86a9467665742b4356a
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:05:36 -0500

    Update mansionb1.asm

--- data/wildPokemon/mansionb1.asm
+++ data/wildPokemon/mansionb1.asm
@@ -1,14 +1,14 @@
 MansionMonsB1:
 	db $0a
-	db 62,VENOMOTH
-	db 62,MR_MIME
-	db 62,MUK
-	db 62,PRIMEAPE
-	db 62,GOLEM
-	db 62,WEEZING
-	db 62,EXEGGUTOR
-	db 62,TANGELA
-	db 62,MUK
-	db 62,MAGMAR
+	db 46,GRIMER	; 20% Grimer 
+	db 46,KOFFING	; 20% Koffing
+	db 51,RATICATE	; 15% Raticate
+	db 47,PONYTA	; 10% Ponyta
+	db 52,MUK	; 10% Muk
+	db 52,WEEZING	; 10% Weezing
+	db 48,GROWLITHE	; 5% Growlithe
+	db 48,VULPIX	; 5% Vulpix
+	db 43,DITTO	; 4% Ditto
+	db 50,MAGMAR	; 1% Magmar
 	db $00
 
