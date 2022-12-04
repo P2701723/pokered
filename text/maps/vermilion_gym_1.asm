a60337f6565a81ff027173a2a78cafea4d3bcdce
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 16:55:58 -0500

    Update vermilion_gym_1.asm

--- text/maps/vermilion_gym_1.asm
+++ text/maps/vermilion_gym_1.asm
@@ -1,5 +1,4 @@
 _VermilionGymText_5cb6d:: ; 9aaa5 (26:6aa5)
-IF DEF(_YELLOW)
 	text "Ten-hut! Welcome"
 	line "to VERMILION GYM!"
 
@@ -7,26 +6,23 @@ IF DEF(_YELLOW)
 	line "that, a pint-size"
 	cont "challenger!"
 
+	para "Hey, kid! What do"
+	line "you think you're"
+	cont "doing here?"
+
+	para "You won't live"
+	line "long in combat,"
+	cont "that's for sure!"
+
 	para "Hahaha! You've got"
 	line "big and brassy"
 	cont "nerves to take me"
 	cont "on with your puny"
 	cont "power!"
-
+	
 	para "A #MON battle"
 	line "is war! I'll show"
 	cont "you, civilian!"
-
-	para "I'll shock you"
-	line "into surrender!"
-ELSE
-	text "Hey, kid! What do"
-	line "you think you're"
-	cont "doing here?"
-
-	para "You won't live"
-	line "long in combat!"
-	cont "That's for sure!"
 
 	para "I tell you kid,"
 	line "electric #MON"
@@ -37,7 +33,6 @@ ELSE
 	line "enemies into"
 	cont "paralysis!"
 
-	para "The same as I'll"
-	line "do to you!"
-ENDC
+	para "We'll shock you"
+	line "into surrender!"
 	done
