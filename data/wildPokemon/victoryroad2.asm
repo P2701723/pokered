a0dbce8574b79146cc3072973f17c6f0f8e0212b
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:33:54 -0500

    Update victoryroad2.asm

--- data/wildPokemon/victoryroad2.asm
+++ data/wildPokemon/victoryroad2.asm
@@ -1,14 +1,14 @@
 PlateauMons2:
 	db $0A
-	db 53,GEODUDE	; 25% Geodude
-	db 61,ONIX	; 20% Onix
-	db 52,MACHOP	; 15% Machop
-	db 51,ZUBAT	; 10% Zubat
-	db 61,GOLBAT	; 10% Golbat
-	db 62,GRAVELER	; 10% Graveler
-	db 53,GEODUDE	; 5% Machoke
-	db 61,MACHOKE	; 4% Venomoth
-	db 60,VENOMOTH	; 1% Hitmonchan
-	db 60,HITMONCHAN
+	db 52,GEODUDE	; 20% Geodude
+	db 51,MACHOP	; 20% Machop
+	db 53,ZUBAT	; 20% Onix
+	db 58,ONIX	; 15% Zubat
+	db 58,ONIX	; 10% Golbat
+	db 60,GOLBAT	; 5% Machoke
+	db 60,MACHOKE	; 5% Graveler
+	db 61,GRAVELER	; 4% Marowak
+	db 60,MAROWAK	; 1% Hitmonlee
+	db 60,HITMONLEE
 	db $00
 
