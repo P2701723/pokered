be072331cf71743c31db7d99c00228b19f3f208f
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 14:48:35 -0500

    Update celadon_hotel.asm

--- text/maps/celadon_hotel.asm
+++ text/maps/celadon_hotel.asm
@@ -9,7 +9,7 @@ _CeladonHotelText1:: ; 9e18a (27:618a)
 _CeladonHotelText2:: ; 9e1bf (27:61bf)
 	text "I'm on vacation"
 	line "with my brother"
-	cont "and boy friend."
+	cont "and boyfriend."
 
 	para "CELADON is such a"
 	line "pretty city!"
@@ -17,6 +17,6 @@ _CeladonHotelText2:: ; 9e1bf (27:61bf)
 
 _CeladonHotelText3:: ; 9e20e (27:620e)
 	text "Why did she bring"
-	line "her brother?"
+	line "her brother...?"
 	done
 
