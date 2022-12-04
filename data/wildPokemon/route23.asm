285d4963015dce51064ddebec2bf0ce7a50e1ec6
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:30:00 -0500

    Update route23.asm

--- data/wildPokemon/route23.asm
+++ data/wildPokemon/route23.asm
@@ -2,13 +2,13 @@ Route23Mons:
 	db $0a
 	db 52,RATICATE	; 20% Raticate
 	db 52,FEAROW	; 20% Fearow
-	db 52,DITTO	; 20% Ditto
+	db 52,DITTO	; 19% Ditto
 	db 52,ARBOK	; 10% Arbok
 	db 52,SANDSLASH	; 10% Sandslash
 	db 52,PRIMEAPE	; 10% Primeape
 	db 56,NIDORINO	; 5% Nidorino
 	db 56,NIDORINA	; 5% Nidorina
-	db 52,DITTO
-	db 52,DITTO
+	db 52,DITTO	; 1% Poliwhirl
+	db 52,POLIWHIRL
 	db $00
 
