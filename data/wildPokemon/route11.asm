827149dcd068485a175317f211240723324dcd17
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:17:43 -0500

    Update route11.asm

--- data/wildPokemon/route11.asm
+++ data/wildPokemon/route11.asm
@@ -1,14 +1,14 @@
 Route11Mons:
 	db $0f
-	db 18,PARAS
-	db 18,MEOWTH
-	db 18,TANGELA
-	db 18,DROWZEE
-	db 18,SPEAROW
-	db 18,JYNX
-	db 18,SLOWBRO
-	db 18,EXEGGCUTE
-	db 18,PINSIR
-	db 18,DITTO
+	db 15,DROWZEE	; 25% Drowzee
+	db 17,SPEAROW	; 20% Spearow
+	db 15,EKANS	; 15% Ekans
+	db 18,PIDGEY	; 15% Sandshrew
+	db 17,RATTATA	; 10% Pidgey
+	db 15,SANDSHREW	; 10% Rattata
+	db 15,SANDSHREW	; 4% Pidgeotto
+	db 19,DROWZEE	; 1% Raticate
+	db 20,PIDGEOTTO
+	db 20,RATICATE
 	db $00
 
