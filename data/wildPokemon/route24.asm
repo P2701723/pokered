f38da9bb94c3a4a4de93ff2dba01ed01a03d7eab
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:30:18 -0500

    Update route24.asm

--- data/wildPokemon/route24.asm
+++ data/wildPokemon/route24.asm
@@ -1,14 +1,14 @@
 Route24Mons:
 	db $11
-	db 18,GROWLITHE
-	db 18,VULPIX
-	db 18,PONYTA
-	db 18,PARAS
-	db 18,MACHOP
-	db 18,ABRA
-	db 18,ODDISH
-	db 18,PIDGEY
-	db 18,FARFETCH_D
-	db 18,MAGNEMITE
+	db 15,ODDISH		; 20% Pidgey
+	db 15,BELLSPROUT	; 20% Oddish
+	db 15,PIDGEY		; 20% Bellsprout
+	db 12,CATERPIE		; 10% Caterpie
+	db 12,WEEDLE		; 10% Weedle
+	db 13,ABRA		; 10% Abra
+	db 16,VENONAT		; 5% Venonat
+	db 17,PIDGEY		; 4% Pidgeotto
+	db 18,PIDGEOTTO		; 1% Squirtle
+	db 15,SQUIRTLE
 	db $00
 
