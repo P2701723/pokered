e43886f65d848680ee4056679c5dd3527edc0bd6
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 18:07:17 -0500

    Update silph_co_5f_2.asm

--- text/maps/silph_co_5f_2.asm
+++ text/maps/silph_co_5f_2.asm
@@ -1,12 +1,12 @@
 _SilphCo5EndBattleText4:: ; 84000 (21:4000)
-	text "Oh"
-	line "goodness!"
+	text "Oh crap!"
 	prompt
 
 _SilphCo5AfterBattleText4:: ; 8400e (21:400e)
 	text "You're only on 5F."
-	line "It's a long way"
-	cont "to my BOSS!"
+	
+	para "It's a long way"
+	line "to my BOSS!"
 	done
 
 _SilphCo5BattleText5:: ; 8403c (21:403c)
@@ -16,7 +16,7 @@ _SilphCo5BattleText5:: ; 8403c (21:403c)
 
 _SilphCo5EndBattleText5:: ; 84060 (21:4060)
 	text "Cough..."
-	line "Cough..."
+	line "Wheeze..."
 	prompt
 
 _SilphCo5AfterBattleText5:: ; 84073 (21:4073)
@@ -49,8 +49,7 @@ _SilphCo5Text11:: ; 84134 (21:4134)
 	text "It's a #MON"
 	line "REPORT!"
 
-	para "4 #MON evolve"
-	line "only when traded"
-	cont "by link-cable."
+	para "SLOWPOKE evolves"
+	line "when traded for"
+	cont "a SHELLDER."
 	done
-
