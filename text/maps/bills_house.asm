278e6bd5c0e8fef5ced00c448bbbdd7dd4932e4a
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 14:29:36 -0500

    Update bills_house.asm

--- text/maps/bills_house.asm
+++ text/maps/bills_house.asm
@@ -12,9 +12,14 @@ _BillsHouseText_1e865:: ; 8d267 (23:5267)
 
 	para "Call me BILL!"
 	line "I'm a true blue"
-	cont "#MANIAC! Hey!"
-	cont "What's with that"
-	cont "skeptical look?"
+	cont "#MANIAC!"
+	
+	para "..."
+	line "..."
+	
+	para "Hey! What's with"
+	line "that skeptical"
+	cont "look?"
 
 	para "I'm not joshing"
 	line "you, I screwed up"
@@ -30,8 +35,8 @@ _BillsHouseText_1e86a:: ; 8d345 (23:5345)
 	text "When I'm in the"
 	line "TELEPORTER, go to"
 	cont "my PC and run the"
-	cont "Cell Separation"
-	cont "System!"
+	cont "CELL SEPARATION"
+	cont "SYSTEM!"
 	done
 
 _BillsHouseText_1e86f:: ; 8d391 (23:5391)
@@ -41,19 +46,27 @@ _BillsHouseText_1e86f:: ; 8d391 (23:5391)
 
 	para "What do you say,"
 	line "chief? Please?"
-	cont "OK? All right!"
+	
+	para "..."
+	line "..."
+	
+	para "OK? All right!"
 	prompt
 
 _BillThankYouText:: ; 8d3f5 (23:53f5)
-	text "BILL: Yeehah!"
-	line "Thanks, bud! I"
-	cont "owe you one!"
+	text "Yeehaw! Thanks"
+	line "a million, bud!"
+	cont "I owe you one!"
 
 	para "So, did you come"
 	line "to see my #MON"
 	cont "collection?"
-	cont "You didn't?"
-	cont "That's a bummer."
+	
+	para "..."
+	line "..."
+	
+	para "You didn't?"
+	line "That's a bummer."
 
 	para "I've got to thank"
 	line "you... Oh here,"
@@ -73,23 +86,25 @@ _SSTicketNoRoomText:: ; 8d4b0 (23:54b0)
 
 _BillsHouseText_1e8cb:: ; 8d4d0 (23:54d0)
 	text "That cruise ship,"
-	line "S.S.ANNE, is in"
+	line "ST.ANNE, is in"
 	cont "VERMILION CITY."
-	cont "Its passengers"
-	cont "are all trainers!"
+	
+	para "Its passengers"
+	line "are all trainers!"
 
 	para "They invited me"
 	line "to their party,"
 	cont "but I can't stand"
-	cont "fancy do's. Why"
-	cont "don't you go"
-	cont "instead of me?"
+	cont "fancy do's."
+	
+	para "Why don't you go"
+	line "instead of me?"
 	done
 
 _BillsHouseText_1e8da:: ; 8d57f (23:557f)
-	text "BILL: Look, bud,"
-	line "just check out"
+	text "Hiya, bud! Feel"
+	line "like checking out"
 	cont "some of my rare"
-	cont "#MON on my PC!"
+	cont "#MON on my PC?"
 	done
 
