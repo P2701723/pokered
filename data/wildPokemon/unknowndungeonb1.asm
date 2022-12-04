a61c47d28111757406aedfefc48075b0af73a6d1
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:36:19 -0500

    Update unknowndungeonb1.asm

--- data/wildPokemon/unknowndungeonb1.asm
+++ data/wildPokemon/unknowndungeonb1.asm
@@ -1,13 +1,13 @@
 DungeonMonsB1:
 	db $10
-	db 85,RHYDON
-	db 85,MAROWAK
-	db 85,ELECTRODE
-	db 85,CHANSEY
-	db 85,PARASECT
-	db 85,RAICHU
-	db 85,ARBOK
-	db 85,GENGAR
-	db 85,SANDSLASH
-	db 88,DITTO
+	db 70,GOLBAT	; 25% Rhydon
+	db 70,MAROWAK	; 20% Golbat
+	db 70,RHYDON	; 20% Marowak
+	db 70,RHYDON	; 10% Ditto
+	db 75,DITTO	; 10% Electrode
+	db 70,ELECTRODE	; 5% Chansey
+	db 75,CHANSEY	; 5% Graveler
+	db 70,GRAVELER	; 4% Lickitung
+	db 70,LICKITUNG	; 1% Snorlax
+	db 70,SNORLAX
 	db $00
