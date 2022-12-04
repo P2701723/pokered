4c546c79456d530791e590a8606e34c378ef49ea
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 18:28:29 -0500

    Update saffron_house.asm

--- text/maps/saffron_house.asm
+++ text/maps/saffron_house.asm
@@ -2,13 +2,16 @@ _SaffronHouse1Text1:: ; a2305 (28:6305)
 	text "Thank you for"
 	line "writing. I hope"
 	cont "to see you soon!"
+	
+	para "..."
+	line "..."
 
 	para "Hey! Don't look"
 	line "at my letter!"
 	done
 
 _SaffronHouse1Text2:: ; a2352 (28:6352)
-	text "PIDGEY: Kurukkoo!@@"
+	text "Kurukkoo!@@"
 
 _SaffronHouse1Text3:: ; a2366 (28:6366)
 	text "The COPYCAT is"
@@ -17,8 +20,8 @@ _SaffronHouse1Text3:: ; a2366 (28:6366)
 	done
 
 _SaffronHouse1Text4:: ; a2395 (28:6395)
-	text "I was given a PP"
-	line "UP as a gift."
+	text "I was given a"
+	line "PP UP as a gift."
 
 	para "It's used for"
 	line "increasing the PP"
