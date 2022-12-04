c35bb03d09301b9ca58c61bc47f5cd576e0bd249
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 14:36:26 -0500

    Update celadon_dept_store_2f.asm

--- text/maps/celadon_dept_store_2f.asm
+++ text/maps/celadon_dept_store_2f.asm
@@ -10,14 +10,14 @@ _CeladonMart2Text3:: ; 9c76b (27:476b)
 _CeladonMart2Text4:: ; 9c7b2 (27:47b2)
 	text "For long outings,"
 	line "you should buy"
-	cont "REVIVE."
+	cont "REVIVEs."
 	done
 
 _CeladonMart2Text5:: ; 9c7dc (27:47dc)
-	text "Top Grade Items"
-	line "for Trainers!"
-
-	para "2F: TRAINER'S"
+	text "2F: TRAINER'S"
 	line "    MARKET"
+	
+	para "Top-grade items"
+	line "for trainers!"
 	done
 
