0a72e50b01029bd1cb0c33e06a5d3e35732aa66a
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:16:28 -0500

    Update route8.asm

--- data/wildPokemon/route8.asm
+++ data/wildPokemon/route8.asm
@@ -1,13 +1,13 @@
 Route8Mons:
 	db $0f
-	db 32,TAUROS
-	db 32,WIGGLYTUFF
-	db 32,CLEFABLE
-	db 32,GYARADOS
-	db 32,SANDSLASH
-	db 32,KADABRA
-	db 32,RAICHU
-	db 32,KANGASKHAN
-	db 32,CHANSEY
-	db 32,DITTO
+	db 25,MANKEY		; 20% Mankey
+	db 25,MEOWTH		; 20% Meowth
+	db 25,PIDGEOTTO		; 15% Pidgeotto
+	db 24,EKANS		; 10% Ekans
+	db 24,SANDSHREW		; 10% Sandshrew
+	db 23,ABRA		; 10% Abra
+	db 23,GROWLITHE		; 5% Growlithe
+	db 23,VULPIX		; 5% Vulpix
+	db 24,JIGGLYPUFF	; 4% Jigglypuff
+	db 27,KADABRA		; 1% Kadabra
 	db $00
