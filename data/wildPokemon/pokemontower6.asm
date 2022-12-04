849ac26b8cc91d523d676928e87316dd2192f15d
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:09:10 -0500

    Update pokemontower6.asm

--- data/wildPokemon/pokemontower6.asm
+++ data/wildPokemon/pokemontower6.asm
@@ -1,14 +1,14 @@
 TowerMons6:
 	db $0F
-	db 55,HAUNTER
-	db 55,MAROWAK
-	db 55,ARBOK
-	db 55,MUK
-	db 55,GASTLY
-	db 55,HAUNTER
-	db 55,HAUNTER
-	db 55,CUBONE
-	db 57,WEEZING
-	db 57,GENGAR
+	db 31,GASTLY	; 75% Gastly L31-37
+	db 32,GASTLY	; 15% Cubone L38
+	db 38,CUBONE	; 10% Haunter L41
+	db 41,HAUNTER
+	db 33,GASTLY
+	db 34,GASTLY
+	db 35,GASTLY
+	db 36,GASTLY
+	db 37,GASTLY
+	db 37,GASTLY
 	db $00
 
