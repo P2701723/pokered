c95272bc53692f2bef3aeb0270b0370f8ee53462
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:08:48 -0500

    Update pokemontower5.asm

--- data/wildPokemon/pokemontower5.asm
+++ data/wildPokemon/pokemontower5.asm
@@ -1,14 +1,14 @@
 TowerMons5:
 	db $0A
-	db 55,HAUNTER
-	db 55,HAUNTER
-	db 55,HAUNTER
-	db 55,GASTLY
-	db 55,GASTLY
-	db 55,GASTLY
-	db 55,HAUNTER
-	db 55,CUBONE
-	db 55,CUBONE
-	db 55,GASTLY
+	db 30,GASTLY	; 75% Gastly L30-36
+	db 31,GASTLY	; 15% Cubone L37
+	db 37,CUBONE	; 10% Haunter L40
+	db 40,HAUNTER
+	db 32,GASTLY
+	db 33,GASTLY
+	db 34,GASTLY
+	db 35,GASTLY
+	db 36,GASTLY
+	db 36,GASTLY
 	db $00
 
