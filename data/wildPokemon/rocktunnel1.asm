30c97c01d69a7f1f851d72bab7667e9a0557253b
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:10:42 -0500

    Update rocktunnel1.asm

--- data/wildPokemon/rocktunnel1.asm
+++ data/wildPokemon/rocktunnel1.asm
@@ -1,14 +1,14 @@
 TunnelMonsB1:
 	db $0F
-	db 36,RHYHORN
-	db 36,MAGMAR
-	db 36,VULPIX
-	db 36,PONYTA
-	db 36,GRAVELER
-	db 36,ZUBAT
-	db 36,SANDSLASH
-	db 36,MACHOP
-	db 36,ONIX
-	db 36,KANGASKHAN
+	db 25,ZUBAT	; 40% Zubat L25
+	db 25,ZUBAT	; 30% Geodude L26
+	db 25,MACHOP	; 20% Machop L25
+	db 26,GEODUDE	; 9% Onix L24
+	db 26,GEODUDE	; 1% Charmander L23
+	db 26,GEODUDE
+	db 25,MACHOP
+	db 24,ONIX
+	db 24,ONIX
+	db 23,CHARMANDER
 	db $00
 
