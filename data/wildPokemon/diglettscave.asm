ff490ac9fc40e1dc1549a29e2a5dbc1d567e03c3
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:01:37 -0500

    Update diglettscave.asm

--- data/wildPokemon/diglettscave.asm
+++ data/wildPokemon/diglettscave.asm
@@ -1,13 +1,13 @@
 CaveMons:
 	db $14
-	db 18,DIGLETT
-	db 19,DIGLETT
-	db 17,DIGLETT
+	db 21,DIGLETT	; 95% Diglett L18-25
+	db 22,DIGLETT	; 5% Dugtrio L29-31
 	db 20,DIGLETT
-	db 16,DIGLETT
-	db 15,DIGLETT
-	db 21,DIGLETT
-	db 22,DIGLETT
+	db 23,DIGLETT
+	db 19,DIGLETT
+	db 18,DIGLETT
+	db 24,DIGLETT
+	db 25,DIGLETT
 	db 29,DUGTRIO
 	db 31,DUGTRIO
 	db $00
