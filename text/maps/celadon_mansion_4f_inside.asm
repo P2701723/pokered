8b5ea0a9ac265073c2d8c5c8e50232bf58e89d64
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 14:51:48 -0500

    Update celadon_mansion_4f_inside.asm

--- text/maps/celadon_mansion_4f_inside.asm
+++ text/maps/celadon_mansion_4f_inside.asm
@@ -1,11 +1,10 @@
 _CeladonMansion5Text1:: ; 9d1ba (27:51ba)
-	text "I know everything"
-	line "about the world"
-	cont "of #MON in"
-	cont "your GAME BOY!"
+	text "The GAME STAFF"
+	line "snuck an extra"
+	cont "#MON into the"
+	cont "game's code!"
 
-	para "Get together with"
-	line "your friends and"
-	cont "trade #MON!"
+	para "I wonder where"
+	line "it's hiding..."
 	done
 
