3ed6fde90c2688e8c92eb75212de68eb2d8e388e
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:19:53 -0500

    Update route17.asm

--- data/wildPokemon/route17.asm
+++ data/wildPokemon/route17.asm
@@ -1,14 +1,14 @@
 Route17Mons:
 	db $19
-	db 41,ARBOK
-	db 41,WEEZING
-	db 41,GOLBAT
-	db 41,HYPNO
-	db 41,MR_MIME
-	db 41,SCYTHER
-	db 41,WEEZING
-	db 41,RATICATE
-	db 41,MUK
-	db 40,HITMONCHAN
+	db 32,DODUO	; 34% Doduo L32-34
+	db 30,SPEAROW	; 25% Spearow L30-31
+	db 33,RATICATE	; 20% Raticate L33-35
+	db 35,FEAROW	; 10% Fearow L35
+	db 34,PONYTA	; 10% Ponyta L34
+	db 33,DODUO	; 1% Dodrio L35
+	db 31,SPEAROW
+	db 35,RATICATE
+	db 34,DODUO
+	db 35,DODRIO
 	db $00
 
