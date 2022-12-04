004361d2f03c7e862adb9c097b5c3deee65a2a62
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 18:05:42 -0500

    Update silph_co_5f_1.asm

--- text/maps/silph_co_5f_1.asm
+++ text/maps/silph_co_5f_1.asm
@@ -2,13 +2,17 @@ _SilphCo5Text_1a010:: ; 82936 (20:6936)
 	text "TEAM ROCKET is"
 	line "in an uproar over"
 	cont "some intruder."
-	cont "That's you right?"
+	
+	para "That's you,"
+	line "right?"
 	done
 
 _SilphCo5Text_1a015:: ; 82978 (20:6978)
 	text "TEAM ROCKET took"
-	line "off! You're our"
-	cont "hero! Thank you!"
+	line "off!"
+	
+	para "You're our hero!"
+	line "Thank you!"
 	done
 
 _SilphCo5BattleText2:: ; 829aa (20:69aa)
@@ -33,8 +37,8 @@ _SilphCo5BattleText3:: ; 82a07 (20:6a07)
 	done
 
 _SilphCo5EndBattleText3:: ; 82a32 (20:6a32)
-	text "Dang!"
-	line "Blast it!"
+	text "Argh!"
+	line "Damn it!"
 	prompt
 
 _SilphCo5AfterBattleText3:: ; 82a43 (20:6a43)
@@ -47,10 +51,6 @@ _SilphCo5AfterBattleText3:: ; 82a43 (20:6a43)
 _SilphCo5BattleText4:: ; 82a81 (20:6a81)
 	text "Whaaat? There"
 	line "shouldn't be any"
-IF DEF(_YELLOW)
 	cont "children here!"
-ELSE
-	cont "children here?"
-ENDC
 	done
 
