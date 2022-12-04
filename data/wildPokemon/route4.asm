2f8ebb539997804f893af5f0645921db01ce2907
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:14:44 -0500

    Update route4.asm

--- data/wildPokemon/route4.asm
+++ data/wildPokemon/route4.asm
@@ -1,14 +1,14 @@
 Route4Mons:
 	db $14
-	db 10,KRABBY
-	db 10,SLOWPOKE ; Hack's mistake. It should be slowpoke. Fixed.
-	db 10,DROWZEE
-	db 10,FARFETCH_D
-	db 10,SPEAROW
-	db 10,DITTO
-	db 10,DODUO
-	db 10,TANGELA
-	db 12,PIDGEY
-	db 10,EKANS
+	db 10,RATTATA		; 34% Spearow L10-12
+	db 10,SPEAROW		; 25% Rattata L10-12
+	db 9,MANKEY		; 15% Mankey L9
+	db 11,SPEAROW		; 15% Sandshrew L10-12
+	db 10,SANDSHREW		; 10% Ekans L10
+	db 10,EKANS		; 1% Charmander L10
+	db 12,SANDSHREW
+	db 12,RATTATA
+	db 12,SPEAROW
+	db 10,CHARMANDER
 	db $00
 
