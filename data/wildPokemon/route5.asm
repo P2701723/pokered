8a9619b36ab224c1c0c0998bc9438dc1ff8d25dd
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:15:07 -0500

    Update route5.asm

--- data/wildPokemon/route5.asm
+++ data/wildPokemon/route5.asm
@@ -1,14 +1,14 @@
 Route5Mons:
 	db $0f
-	db 19,GRIMER
-	db 19,DRATINI
-	db 19,SPEAROW
-	db 19,DODUO
-	db 19,MANKEY
-	db 19,BELLSPROUT
-	db 19,NIDORINO
-	db 19,NIDORINA
-	db 19,HORSEA
-	db 19,GLOOM
+	db 16,PIDGEY		; 20% Pidgey
+	db 16,RATTATA		; 20% Rattata
+	db 16,ODDISH		; 15% Oddish
+	db 14,MANKEY		; 15% Bellsprout
+	db 14,MEOWTH		; 10% Mankey
+	db 16,BELLSPROUT	; 10% Meowth
+	db 16,BELLSPROUT	; 5% Abra
+	db 13,ABRA		; 4% Jigglypuff
+	db 13,JIGGLYPUFF	; 1% Pidgeotto
+	db 18,PIDGEOTTO
 	db $00
 
