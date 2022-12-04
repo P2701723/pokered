1744da7d8540bb650ea2774c99868ea12a8bf5d6
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 16:54:49 -0500

    Update vermilion_city.asm

--- text/maps/vermilion_city.asm
+++ text/maps/vermilion_city.asm
@@ -8,27 +8,27 @@ _VermilionCityText1:: ; a5668 (29:5668)
 	done
 
 _VermilionCityText_198a7:: ; a56b5 (29:56b5)
-	text "Did you see S.S."
-	line "ANNE moored in"
-	cont "the harbor?"
+	text "Did you see the"
+	line "ST.ANNE moored"
+	cont "in the harbor?"
 	done
 
 _VermilionCityText_198ac:: ; a56e2 (29:56e2)
-	text "So, S.S.ANNE has"
-	line "departed!"
+	text "So, the ST.ANNE"
+	line "has departed!"
 
 	para "She'll be back in"
 	line "about a year."
 	done
 
 _SSAnneWelcomeText4:: ; a571d (29:571d)
-	text "Welcome to S.S."
-	line "ANNE!"
+	text "Welcome to the"
+	line "ST.ANNE!"
 	done
 
 _SSAnneWelcomeText9:: ; a5734 (29:5734)
-	text "Welcome to S.S."
-	line "ANNE!"
+	text "Welcome to the"
+	line "ST.ANNE!"
 
 	para "Excuse me, do you"
 	line "have a ticket?"
@@ -39,7 +39,7 @@ _SSAnneFlashedTicketText:: ; a576c (29:576c)
 	line "the S.S.TICKET!"
 
 	para "Great! Welcome to"
-	line "S.S.ANNE!"
+	line "the ST.ANNE!"
 	done
 
 _SSAnneNoTicketText:: ; a57a3 (29:57a3)
@@ -67,19 +67,18 @@ _VermilionCityText4:: ; a5805 (29:5805)
 	done
 
 _VermilionCityText5:: ; a5852 (29:5852)
-	text "MACHOP: Guoh!"
-	line "Gogogoh!@@"
+	text "Guoh! Gogogoh!@@"
 
 _VermilionCityText14:: ; a586b (29:586b)
 	db $0
-	para "A MACHOP is"
+	para "This MACHOP is"
 	line "stomping the land"
 	cont "flat."
 	done
 
 _VermilionCityText6:: ; a5891 (29:5891)
-	text "S.S.ANNE is a"
-	line "famous luxury"
+	text "The ST.ANNE is"
+	line "a famous luxury"
 	cont "cruise ship."
 
 	para "We visit VERMILION"
@@ -88,8 +87,9 @@ _VermilionCityText6:: ; a5891 (29:5891)
 
 _VermilionCityText7:: ; a58db (29:58db)
 	text "VERMILION CITY"
-	line "The Port of"
-	cont "Exquisite Sunsets"
+
+	para "Port of exquisite"
+	line "sunsets!"
 	done
 
 _VermilionCityText8:: ; a5909 (29:5909)
@@ -108,8 +108,9 @@ _VermilionCityText8:: ; a5909 (29:5909)
 
 _VermilionCityText11:: ; a5980 (29:5980)
 	text "#MON FAN CLUB"
-	line "All #MON fans"
-	cont "welcome!"
+
+	para "All #MON fans"
+	line "welcome!"
 	done
 
 _VermilionCityText12:: ; a59a6 (29:59a6)
@@ -117,8 +118,8 @@ _VermilionCityText12:: ; a59a6 (29:59a6)
 	line "#MON GYM"
 	cont "LEADER: LT.SURGE"
 
-	para "The Lightning "
-	line "American!"
+	para "The lightning"
+	line "AMERICAN!"
 	done
 
 _VermilionCityText13:: ; a59e9 (29:59e9)
