16b1b71f9db2413e7ba042f6c7f1048a7cb421a3
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:19:34 -0500

    Update route16.asm

--- data/wildPokemon/route16.asm
+++ data/wildPokemon/route16.asm
@@ -1,14 +1,14 @@
 Route16Mons:
 	db $19
-	db 40,GOLEM
-	db 40,NIDOQUEEN
-	db 40,NIDOKING
-	db 40,PSYDUCK
-	db 40,POLIWAG
-	db 40,RATTATA
-	db 40,RHYHORN
-	db 40,DODRIO
-	db 40,SLOWPOKE
-	db 40,HITMONLEE
+	db 30,SPEAROW	; 35% Spearow L30-32
+	db 29,DODUO	; 30% Doduo L29-30
+	db 29,RATTATA	; 25% Rattata L29-30
+	db 31,SPEAROW	; 5% Raticate L32-33
+	db 30,DODUO	; 5% Fearow L32
+	db 30,RATTATA
+	db 32,SPEAROW
+	db 32,FEAROW
+	db 32,RATICATE
+	db 33,RATICATE
 	db $00
 
