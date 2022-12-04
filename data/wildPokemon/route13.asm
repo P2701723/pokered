0876d63dc22ed794720cf8770db911ac37b8a4a4
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:18:33 -0500

    Update route13.asm

--- data/wildPokemon/route13.asm
+++ data/wildPokemon/route13.asm
@@ -1,14 +1,14 @@
 Route13Mons:
 	db $14
-	db 40,VENOMOTH
-	db 40,VICTREEBEL
-	db 40,PIDGEOT
-	db 40,DODRIO
-	db 40,MUK
-	db 40,WEEZING
-	db 40,ARBOK
-	db 40,SCYTHER
-	db 40,GLOOM
-	db 40,MAGNETON
+	db 31,ODDISH		; 20% Pidgey
+	db 31,BELLSPROUT	; 20% Oddish
+	db 32,VENONAT		; 20% Bellsprout
+	db 32,PIDGEY		; 15% Venonat
+	db 33,PIDGEY		; 10% Pidgeotto
+	db 34,PIDGEOTTO		; 5% Gloom
+	db 35,GLOOM		; 5% Weepinbell
+	db 35,WEEPINBELL	; 4% Ditto
+	db 32,DITTO		; 1% Farfetch'd
+	db 33,FARFETCH_D
 	db $00
 
