ffada2f5c2ca03b0971c650c89a01acba3a93df5
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 12:33:35 -0500

    Update victoryroad3.asm

--- data/wildPokemon/victoryroad3.asm
+++ data/wildPokemon/victoryroad3.asm
@@ -1,14 +1,14 @@
 PlateauMons3:
 	db $0f
-	db 81,ONIX
-	db 81,KANGASKHAN
-	db 81,GOLBAT
-	db 81,BLASTOISE
-	db 81,VENOMOTH
-	db 81,GOLEM
-	db 81,MACHAMP
-	db 81,MAROWAK
-	db 81,CLEFABLE
-	db 81,CHANSEY
+	db 53,GEODUDE	; 25% Geodude
+	db 61,ONIX	; 20% Onix
+	db 52,MACHOP	; 15% Machop
+	db 51,ZUBAT	; 10% Zubat
+	db 61,GOLBAT	; 10% Golbat
+	db 62,GRAVELER	; 10% Graveler
+	db 53,GEODUDE	; 5% Machoke
+	db 61,MACHOKE	; 4% Venomoth
+	db 60,VENOMOTH	; 1% Hitmonchan
+	db 60,HITMONCHAN
 	db $00
 
