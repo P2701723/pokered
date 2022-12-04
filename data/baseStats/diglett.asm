e0294bc307cc736bd2b68a0878ca3ea2ec81a6a4
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-03 21:10:50 -0500

    Update diglett.asm

--- data/baseStats/diglett.asm
+++ data/baseStats/diglett.asm
@@ -1,9 +1,9 @@
 DiglettBaseStats: ; 3893a (e:493a)
 db DEX_DIGLETT ; pokedex id
 db 10 ; base hp
-db 55 ; base attack
-db 25 ; base defense
-db 95 ; base speed
+db 55 ; base attack	+10
+db 30 ; base defense	+5
+db 100 ; base speed	+5
 db 45 ; base special
 db GROUND ; species type 1
 db GROUND ; species type 2
@@ -25,9 +25,5 @@ db %00001000
 db %11001110
 db %00000010
 db %10001000
-IF DEF(_YELLOW)
-	db %00000110
-ELSE
-	db %00000010
-ENDC
+db %00000110 ; Cut
 db 0 ; padding
