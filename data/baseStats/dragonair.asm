21f013c3714789389227adc96a2ae99b3f3be65d
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 23:11:17 -0500

    Update dragonair.asm

--- data/baseStats/dragonair.asm
+++ data/baseStats/dragonair.asm
@@ -1,10 +1,10 @@
 DragonairBaseStats: ; 393f2 (e:53f2)
 db DEX_DRAGONAIR ; pokedex id
-db 61 ; base hp
-db 84 ; base attack
-db 65 ; base defense
-db 70 ; base speed
-db 70 ; base special
+db 71 ; base hp       +10
+db 94 ; base attack   +10
+db 75 ; base defense  +10
+db 80 ; base speed    +10
+db 80 ; base special  +10
 db DRAGON ; species type 1
 db DRAGON ; species type 2
 db 45 ; catch rate
@@ -13,17 +13,17 @@ INCBIN "pic/bmon/dragonair.pic",0,1 ; 66, sprite dimensions
 dw DragonairPicFront
 dw DragonairPicBack
 ; attacks known at lvl 0
-db WRAP
 db LEER
+db WRAP
 db THUNDER_WAVE
 db 0
 db 5 ; growth rate
 ; learnset
 db %11100000
-db %00111111
+db %01111111 ; Hyper Beam
 db %11001000
 db %11000001
-db %11100011
+db %11110011 ; Flamethrower
 db %00011000
 db %00010010
 db 0 ; padding
