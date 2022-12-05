e4da03a6bbfaa6279d3d10d2d8c42c21cad15015
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 23:29:15 -0500

    Update aerodactyl.asm

--- data/baseStats/aerodactyl.asm
+++ data/baseStats/aerodactyl.asm
@@ -1,10 +1,10 @@
 AerodactylBaseStats: ; 3934a (e:534a)
 db DEX_AERODACTYL ; pokedex id
 db 80 ; base hp
-db 105 ; base attack
-db 65 ; base defense
-db 130 ; base speed
-db 60 ; base special
+db 120 ; base attack  +15
+db 75 ; base defense  +10
+db 140 ; base speed   +10
+db 75 ; base special  +15
 db ROCK ; species type 1
 db FLYING ; species type 2
 db 45 ; catch rate
@@ -22,8 +22,8 @@ db 5 ; growth rate
 db %00101010
 db %01000011
 db %01001000
-db %11000000
-db %01100011
-db %00001100
-db %00001010
+db %11000110 ; Earthquake, Fissure
+db %01110011 ; Flamethrower
+db %10001100 ; Rock Slide
+db %00101010 ; Strength
 db 0 ; padding
