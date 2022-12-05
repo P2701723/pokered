c4dfe3aa22fa868e00dca7476ced0ba874023328
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 22:45:18 -0500

    Update mewtwo.asm

--- data/baseStats/mewtwo.asm
+++ data/baseStats/mewtwo.asm
@@ -2,7 +2,7 @@ MewtwoBaseStats: ; 3942a (e:542a)
 db DEX_MEWTWO ; pokedex id
 db 106 ; base hp
 db 110 ; base attack
-db 90 ; base defense
+db 100 ; base defense  +10
 db 130 ; base speed
 db 154 ; base special
 db PSYCHIC ; species type 1
@@ -13,17 +13,17 @@ INCBIN "pic/bmon/mewtwo.pic",0,1 ; 77, sprite dimensions
 dw MewtwoPicFront
 dw MewtwoPicBack
 ; attacks known at lvl 0
-db CONFUSION
-db DISABLE
-db SWIFT
 db PSYCHIC_M
+db BARRIER
+db RECOVER
+db DISABLE
 db 5 ; growth rate
 ; learnset
 db %10110001
 db %11111111
 db %10101111
-db %11110001
-db %10101111
-db %00111000
+db %11110111 ; Earthquake, Fissure
+db %11111111 ; Flamethrower, Swift
+db %10111010 ; Dream Eater, Rock Slide
 db %01100011
 db 0 ; padding
