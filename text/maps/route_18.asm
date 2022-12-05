21c771b2d236492602b84e1441588f5daea280fc
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-04 19:44:23 -0500

    Update route_18.asm

--- text/maps/route_18.asm
+++ text/maps/route_18.asm
@@ -10,18 +10,19 @@ _Route18EndBattleText1:: ; 917d1 (24:57d1)
 
 _Route18AfterBattleText1:: ; 917d7 (24:57d7)
 	text "I wish I had a"
-	line "BIKE!"
+	line "bike!"
 	done
 
 _Route18BattleText2:: ; 917ed (24:57ed)
 	text "Kurukkoo!"
-	line "How do you like"
-	cont "my bird call?"
+	
+	para "How do you like"
+	line "my bird call?"
 	done
 
 _Route18EndBattleText2:: ; 91816 (24:5816)
-	text "I"
-	line "had to bug you!"
+	text ""
+	line "I had to bug you!"
 	prompt
 
 _Route18AfterBattleText2:: ; 91829 (24:5829)
@@ -36,7 +37,7 @@ _Route18BattleText3:: ; 9184f (24:584f)
 	done
 
 _Route18EndBattleText3:: ; 91872 (24:5872)
-	text "Darn!"
+	text "Damn!"
 	prompt
 
 _Route18AfterBattleText3:: ; 91879 (24:5879)
@@ -47,13 +48,15 @@ _Route18AfterBattleText3:: ; 91879 (24:5879)
 
 _Route18Text4:: ; 9189d (24:589d)
 	text "ROUTE 18"
-	line "CELADON CITY -"
-	cont "FUCHSIA CITY"
+	
+	para "CELADON CITY -"
+	line "FUCHSIA CITY"
 	done
-
+	
 _Route18Text5:: ; 918c3 (24:58c3)
 	text "CYCLING ROAD"
-	line "No pedestrians"
-	cont "permitted!"
+	
+	para "No pedestrians"
+	line "permitted!"
 	done
 
