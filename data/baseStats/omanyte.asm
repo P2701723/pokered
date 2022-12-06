bdb163064d3028288626aaecaf8aaffe079ef361
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-06 18:55:34 -0500

    Update omanyte.asm

--- data/baseStats/omanyte.asm
+++ data/baseStats/omanyte.asm
@@ -1,10 +1,10 @@
 OmanyteBaseStats: ; 392da (e:52da)
 db DEX_OMANYTE ; pokedex id
-db 35 ; base hp
-db 40 ; base attack
-db 100 ; base defense
-db 35 ; base speed
-db 90 ; base special
+db 40 ; base hp         +5
+db 45 ; base attack     +5
+db 105 ; base defense   +5
+db 40 ; base speed      +5
+db 95 ; base special    +5
 db ROCK ; species type 1
 db WATER ; species type 2
 db 45 ; catch rate
@@ -24,6 +24,6 @@ db %00111111
 db %00001000
 db %11000000
 db %00000011
-db %00001000
+db %10001000 ; Rock Slide
 db %00010010
 db 0 ; padding
