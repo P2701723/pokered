8722ec0862df686edcb275dc4c2a442dafa2e809
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-06 18:50:40 -0500

    Update kabutops.asm

--- data/baseStats/kabutops.asm
+++ data/baseStats/kabutops.asm
@@ -1,10 +1,10 @@
 KabutopsBaseStats: ; 3932e (e:532e)
 db DEX_KABUTOPS ; pokedex id
-db 60 ; base hp
-db 115 ; base attack
-db 105 ; base defense
-db 80 ; base speed
-db 70 ; base special
+db 70 ; base hp
+db 125 ; base attack    +10
+db 115 ; base defense   +10
+db 90 ; base speed      +10
+db 75 ; base special    +5
 db ROCK ; species type 1
 db WATER ; species type 2
 db 45 ; catch rate
@@ -13,17 +13,17 @@ INCBIN "pic/bmon/kabutops.pic",0,1 ; 66, sprite dimensions
 dw KabutopsPicFront
 dw KabutopsPicBack
 ; attacks known at lvl 0
-db SCRATCH
-db HARDEN
-db ABSORB
+db SLASH
+db MEGA_DRAIN
+db ROCK_SLIDE
 db 0
 db 0 ; growth rate
 ; learnset
 db %10110110
 db %01111111
-db %00001101
-db %11000000
+db %00011101 ; Mega Drain
+db %11001000 ; Dig
 db %10000011
-db %00001000
-db %00010010
+db %10001000 ; Rock Slide
+db %00010110 ; Cut
 db 0 ; padding
