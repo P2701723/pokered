fe0678fd727d1a3234b0b1c7f38b52cf1588942c
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-06 22:33:01 -0500

    Update bellsprout.asm

--- data/baseStats/bellsprout.asm
+++ data/baseStats/bellsprout.asm
@@ -1,9 +1,9 @@
 BellsproutBaseStats: ; 38b4e (e:4b4e)
 db DEX_BELLSPROUT ; pokedex id
-db 50 ; base hp
+db 55 ; base hp       +5
 db 75 ; base attack
 db 35 ; base defense
-db 40 ; base speed
+db 50 ; base speed    +10
 db 70 ; base special
 db GRASS ; species type 1
 db POISON ; species type 2
@@ -25,5 +25,5 @@ db %00111000
 db %11000000
 db %00000011
 db %00001000
-db %00000110
+db %01000110 ; Flash
 db 0 ; padding
