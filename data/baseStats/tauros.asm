460824bac94ad57f9c00592a1ff1e202a21ab410
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-06 20:03:58 -0500

    Update tauros.asm

--- data/baseStats/tauros.asm
+++ data/baseStats/tauros.asm
@@ -2,9 +2,9 @@ TaurosBaseStats: ; 391c2 (e:51c2)
 db DEX_TAUROS ; pokedex id
 db 75 ; base hp
 db 100 ; base attack
-db $69 ; base defense
+db 95 ; base defense
 db 110 ; base speed
-db $5a ; base special
+db 70 ; base special
 db NORMAL ; species type 1
 db NORMAL ; species type 2
 db 45 ; catch rate
@@ -20,10 +20,10 @@ db 0
 db 5 ; growth rate
 ; learnset
 db %11100000
-db %01110011
-db %10001000
+db %01111111 ; Bubblebeam, Water Gun
+db %10101000 ; Solarbeam
 db %11000111
-db %10100010
-db %00001000
-db %00100010
+db %10110010 ; Flamethrower
+db %10001000 ; Rock Slide
+db %00110010 ; Surf
 db 0 ; padding
