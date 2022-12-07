a0a6f85514218c0e29f0561807fdd4fe8c2b7901
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-06 19:18:47 -0500

    Update vaporeon.asm

--- data/baseStats/vaporeon.asm
+++ data/baseStats/vaporeon.asm
@@ -1,9 +1,9 @@
 VaporeonBaseStats: ; 3926a (e:526a)
 db DEX_VAPOREON ; pokedex id
 db 130 ; base hp
-db 65 ; base attack
-db $46 ; base defense
-db $4b ; base speed
+db 75 ; base attack	+10
+db 70 ; base defense	+10
+db 85 ; base speed	+20
 db 110 ; base special
 db WATER ; species type 1
 db WATER ; species type 2
@@ -13,12 +13,8 @@ INCBIN "pic/bmon/vaporeon.pic",0,1 ; 66, sprite dimensions
 dw VaporeonPicFront
 dw VaporeonPicBack
 ; attacks known at lvl 0
-db TACKLE
-IF DEF(_YELLOW)
-	db TAIL_WHIP
-ELSE
-	db SAND_ATTACK
-ENDC
+db TAIL_WHIP
+db SAND_ATTACK
 db QUICK_ATTACK
 db WATER_GUN
 db 0 ; growth rate
@@ -26,8 +22,8 @@ db 0 ; growth rate
 db %10100000
 db %01111111
 db %00001000
-db %11000000
+db %11001000 ; Dig
 db %11000011
 db %00001000
-db %00010010
+db %00110010 ; Strength
 db 0 ; padding
