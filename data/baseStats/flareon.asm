d6dc321b5c4467ecae14c999be3deb50e9db952a
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-06 19:22:20 -0500

    Update flareon.asm

--- data/baseStats/flareon.asm
+++ data/baseStats/flareon.asm
@@ -1,9 +1,9 @@
 FlareonBaseStats: ; 392a2 (e:52a2)
 db DEX_FLAREON ; pokedex id
-db $4b ; base hp
+db 85 ; base hp		+20
 db 130 ; base attack
-db $46 ; base defense
-db 65 ; base speed
+db 70 ; base defense	+10
+db 75 ; base speed	+10
 db 110 ; base special
 db FIRE ; species type 1
 db FIRE ; species type 2
@@ -13,12 +13,8 @@ INCBIN "pic/bmon/flareon.pic",0,1 ; 66, sprite dimensions
 dw FlareonPicFront
 dw FlareonPicBack
 ; attacks known at lvl 0
-db TACKLE
-IF DEF(_YELLOW)
-	db TAIL_WHIP
-ELSE
-	db SAND_ATTACK
-ENDC
+db TAIL_WHIP
+db SAND_ATTACK
 db QUICK_ATTACK
 db EMBER
 db 0 ; growth rate
@@ -26,8 +22,8 @@ db 0 ; growth rate
 db %10100000
 db %01000011
 db %00001000
-db %11000000
-db %11100011
+db %11001000 ; Dig
+db %11110011 ; Flamethrower
 db %00001000
-db %00000010
+db %00100010 ; Strength
 db 0 ; padding
