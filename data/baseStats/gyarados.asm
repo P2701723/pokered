27f781302ce48cb1dc9b5b598e246367ce4874af
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-06 19:40:01 -0500

    Update gyarados.asm

--- data/baseStats/gyarados.asm
+++ data/baseStats/gyarados.asm
@@ -1,10 +1,10 @@
 GyaradosBaseStats: ; 391fa (e:51fa)
 db DEX_GYARADOS ; pokedex id
 db 95 ; base hp
-db 125 ; base attack
-db 79 ; base defense
+db 140 ; base attack  +15
+db 94 ; base defense  +15
 db 81 ; base speed
-db 100 ; base special
+db 90 ; base special  -10
 db WATER ; species type 1
 db FLYING ; species type 2
 db 45 ; catch rate
@@ -13,24 +13,17 @@ INCBIN "pic/bmon/gyarados.pic",0,1 ; 77, sprite dimensions
 dw GyaradosPicFront
 dw GyaradosPicBack
 ; attacks known at lvl 0
-IF DEF(_YELLOW)
-db TACKLE
+db THRASH
 db 0
 db 0
 db 0
-ELSE
-db BITE
-db DRAGON_RAGE
-db LEER
-db HYDRO_PUMP
-ENDC
 db 5 ; growth rate
 ; learnset
 db %10100000
 db %01111111
 db %11001000
-db %11000001
-db %10100011
-db %00001000
+db %11000111 ; Earthquake, Fissure
+db %10110011 ; Flamethrower
+db %00011000 ; Thunder Wave
 db %00110010
 db 0 ; padding
