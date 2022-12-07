c3e31c24e4cd83d6946e43d0bff4f8254a963f87
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-06 22:56:05 -0500

    Update ponyta.asm

--- data/baseStats/ponyta.asm
+++ data/baseStats/ponyta.asm
@@ -2,9 +2,9 @@ PonytaBaseStats: ; 38c2e (e:4c2e)
 db DEX_PONYTA ; pokedex id
 db 50 ; base hp
 db 85 ; base attack
-db 55 ; base defense
-db 90 ; base speed
-db 65 ; base special
+db 60 ; base defense  +5
+db 95 ; base speed    +5
+db 75 ; base special  +10
 db FIRE ; species type 1
 db FIRE ; species type 2
 db 190 ; catch rate
@@ -21,9 +21,9 @@ db 0 ; growth rate
 ; learnset
 db %11100000
 db %00000011
-db %00001000
+db %00101000 ; Solarbeam
 db %11000000
-db %11100011
+db %11110011 ; Flamethrower
 db %00001000
-db %00000010
+db %00100010 ; Strength
 db 0 ; padding
