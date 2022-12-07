6d310bb3e3b70bd4e5ffcb0716357df3bd230b23
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-06 22:29:36 -0500

    Update victreebel.asm

--- data/baseStats/victreebel.asm
+++ data/baseStats/victreebel.asm
@@ -1,9 +1,9 @@
 VictreebelBaseStats: ; 38b86 (e:4b86)
 db DEX_VICTREEBEL	 ; pokedex id
-db 80 ; base hp
+db 85 ; base hp       +5
 db 105 ; base attack
 db 65 ; base defense
-db 70 ; base speed
+db 90 ; base speed    +20
 db 100 ; base special
 db GRASS ; species type 1
 db POISON ; species type 2
@@ -13,10 +13,10 @@ INCBIN "pic/bmon/victreebel.pic",0,1 ; 77, sprite dimensions
 dw VictreebelPicFront
 dw VictreebelPicBack
 ; attacks known at lvl 0
-db SLEEP_POWDER
-db STUN_SPORE
-db ACID
 db RAZOR_LEAF
+db WRAP
+db POISONPOWDER
+db SLEEP_POWDER
 db 3 ; growth rate
 ; learnset
 db %10100100
@@ -25,5 +25,5 @@ db %00111000
 db %11000000
 db %00000011
 db %00001000
-db %00000110
+db %01000110 ; Flash
 db 0 ; padding
