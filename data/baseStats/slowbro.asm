068bfad75ca6ceb80d406783ab84c43200030429
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-06 22:19:08 -0500

    Update slowbro.asm

--- data/baseStats/slowbro.asm
+++ data/baseStats/slowbro.asm
@@ -4,7 +4,7 @@ db 95 ; base hp
 db 75 ; base attack
 db 110 ; base defense
 db 30 ; base speed
-db 80 ; base special
+db 100 ; base special +20
 db WATER ; species type 1
 db PSYCHIC ; species type 2
 db 75 ; catch rate
@@ -13,17 +13,17 @@ INCBIN "pic/bmon/slowbro.pic",0,1 ; 77, sprite dimensions
 dw SlowbroPicFront
 dw SlowbroPicBack
 ; attacks known at lvl 0
-db CONFUSION
 db DISABLE
 db HEADBUTT
-db 0
+db CONFUSION
+db WATER_GUN
 db 0 ; growth rate
 ; learnset
 db %10110001
 db %11111111
 db %00001111
 db %11111110
-db %11100011
-db %00111000
+db %11110011 ; Flamethrower
+db %00111010 ; Dream Eater
 db %01110011
 db 0 ; padding
