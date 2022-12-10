1fcd6d3e47412973f17f03555c2e5416742811a9
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-10 12:01:59 -0500

    Update paras.asm

--- data/baseStats/paras.asm
+++ data/baseStats/paras.asm
@@ -1,10 +1,10 @@
 ParasBaseStats: ; 388ca (e:48ca)
 db DEX_PARAS ; pokedex id
-db $32 ; base hp
-db $5a ; base attack
-db $4b ; base defense
-db $2d ; base speed
-db $3c ; base special
+db 45 ; base hp       +10
+db 80 ; base attack   +10
+db 65 ; base defense  +10
+db 30 ; base speed    +5
+db 65 ; base special  +10
 db BUG ; species type 1
 db GRASS ; species type 2
 db 190 ; catch rate
@@ -21,9 +21,9 @@ db 0 ; growth rate
 ; learnset
 db %10100100
 db %00000011
-db %00111000
+db %00111010 ; Counter
 db %11001000
 db %10000011
 db %00001000
-db %00000110
+db %01000110 ; Flash
 db 0 ; padding
