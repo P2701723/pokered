46397185b2123bc9151547d148a85cf22c1c4b27
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-10 12:02:07 -0500

    Update parasect.asm

--- data/baseStats/parasect.asm
+++ data/baseStats/parasect.asm
@@ -1,10 +1,10 @@
 ParasectBaseStats: ; 388e6 (e:48e6)
 db DEX_PARASECT ; pokedex id
-db $64 ; base hp
-db $73 ; base attack
-db $73 ; base defense
-db $64 ; base speed
-db $64 ; base special
+db 80 ; base hp       +20
+db 115 ; base attack  +20
+db 100 ; base defense +20
+db 40 ; base speed    +10
+db 100 ; base special +20
 db BUG ; species type 1
 db GRASS ; species type 2
 db 75 ; catch rate
@@ -14,16 +14,16 @@ dw ParasectPicFront
 dw ParasectPicBack
 ; attacks known at lvl 0
 db SCRATCH
+db POISONPOWDER
 db STUN_SPORE
 db LEECH_LIFE
-db 0
 db 0 ; growth rate
 ; learnset
 db %10100100
 db %01000011
-db %00111000
+db %00111010 ; Counter
 db %11001000
 db %10000011
 db %00001000
-db %00000110
+db %01000110 ; Flash
 db 0 ; padding
