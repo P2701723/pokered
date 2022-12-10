6a10ef7ee9919e6eb6f1580b5f2b888b23cfc9d5
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-10 14:04:38 -0500

    Update poliwrath.asm

--- data/baseStats/poliwrath.asm
+++ data/baseStats/poliwrath.asm
@@ -1,10 +1,10 @@
 PoliwrathBaseStats: ; 38a8a (e:4a8a)
 db DEX_POLIWRATH ; pokedex id
 db 90 ; base hp
-db 85 ; base attack
-db 95 ; base defense
+db 105 ; base attack  +20
+db 105 ; base defense +10
 db 70 ; base speed
-db 70 ; base special
+db 90 ; base special  +20
 db WATER ; species type 1
 db FIGHTING ; species type 2
 db 45 ; catch rate
@@ -13,16 +13,16 @@ INCBIN "pic/bmon/poliwrath.pic",0,1 ; 77, sprite dimensions
 dw PoliwrathPicFront
 dw PoliwrathPicBack
 ; attacks known at lvl 0
+db DOUBLESLAP
 db HYPNOSIS
 db WATER_GUN
-db DOUBLESLAP
-db BODY_SLAM
+db SUBMISSION
 db 3 ; growth rate
 ; learnset
 db %10110001
 db %01111111
 db %00001111
-db %11010110
+db %11011110 ; Dig
 db %10000110
 db %00101000
 db %00110010
