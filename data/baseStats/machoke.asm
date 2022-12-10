2355c1d11489e58c2931e7fe693469a182dbef8b
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-10 14:32:48 -0500

    Update machoke.asm

--- data/baseStats/machoke.asm
+++ data/baseStats/machoke.asm
@@ -1,10 +1,10 @@
 MachokeBaseStats: ; 38b16 (e:4b16)
 db DEX_MACHOKE ; pokedex id
 db 80 ; base hp
-db 100 ; base attack
+db 110 ; base attack  +10
 db 70 ; base defense
-db 45 ; base speed
-db 50 ; base special
+db 55 ; base speed    +10
+db 60 ; base special  +10
 db FIGHTING ; species type 1
 db FIGHTING ; species type 2
 db 90 ; catch rate
@@ -13,9 +13,9 @@ INCBIN "pic/bmon/machoke.pic",0,1 ; 77, sprite dimensions
 dw MachokePicFront
 dw MachokePicBack
 ; attacks known at lvl 0
-db KARATE_CHOP
-db LOW_KICK
 db LEER
+db LOW_KICK
+db KARATE_CHOP
 db 0
 db 3 ; growth rate
 ; learnset
@@ -23,7 +23,7 @@ db %10110001
 db %00000011
 db %00001111
 db %11001110
-db %10100110
+db %10110110 ; Flamethrower
 db %10001000
 db %00100010
 db 0 ; padding
