92a843cff0f1423e40cfff7fcfe761ba790219d9
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-10 11:47:20 -0500

    Update golbat.asm

--- data/baseStats/golbat.asm
+++ data/baseStats/golbat.asm
@@ -1,10 +1,10 @@
 GolbatBaseStats: ; 3885a (e:485a)
 db DEX_GOLBAT ; pokedex id
-db 75 ; base hp
-db 80 ; base attack
-db 70 ; base defense
-db 90 ; base speed
-db 75 ; base special
+db 85 ; base hp       +10
+db 90 ; base attack   +10
+db 80 ; base defense  +10
+db 130 ; base speed   +40
+db 80 ; base special  +5
 db POISON ; species type 1
 db FLYING ; species type 2
 db 90 ; catch rate
@@ -13,10 +13,10 @@ INCBIN "pic/bmon/golbat.pic",0,1 ; 77, sprite dimensions
 dw GolbatPicFront
 dw GolbatPicBack
 ; attacks known at lvl 0
-db LEECH_LIFE
+db SUPERSONIC
 db SCREECH
+db LEECH_LIFE
 db BITE
-db 0
 db 0 ; growth rate
 ; learnset
 db %00101010
@@ -24,6 +24,6 @@ db %01000011
 db %00011000
 db %11000000
 db %01000010
-db %00001000
-db %00000010
+db %00001100 ; Sky Attack
+db %00001010 ; Fly
 db 0 ; padding
