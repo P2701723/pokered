eb675d5d6e6a6a73a5b07e5cc79e1faa1a228eb8
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-10 11:38:18 -0500

    Update wigglytuff.asm

--- data/baseStats/wigglytuff.asm
+++ data/baseStats/wigglytuff.asm
@@ -1,10 +1,10 @@
 WigglytuffBaseStats: ; 38822 (e:4822)
 db DEX_WIGGLYTUFF ; pokedex id
-db 140 ; base hp
-db 70 ; base attack
-db 45 ; base defense
-db 45 ; base speed
-db 50 ; base special
+db 150 ; base hp      +10
+db 80 ; base attack   +10
+db 55 ; base defense  +10
+db 55 ; base speed    +10
+db 95 ; base special  +45
 db NORMAL ; species type 1
 db NORMAL ; species type 2
 db 50 ; catch rate
@@ -13,17 +13,17 @@ INCBIN "pic/bmon/wigglytuff.pic",0,1 ; 66, sprite dimensions
 dw WigglytuffPicFront
 dw WigglytuffPicBack
 ; attacks known at lvl 0
-db SING
-db DISABLE
-db DEFENSE_CURL
 db DOUBLESLAP
+db REST
+db BODY_SLAM
+db DOUBLE_EDGE
 db 4 ; growth rate
 ; learnset
 db %10110001
 db %01111111
 db %10101111
-db %11110001
-db %10100011
-db %00111000
+db %11111001 ; Dig
+db %10110011 ; Flamethrower
+db %00111011 ; Dream Eater, Softboiled
 db %01100011
 db 0 ; padding
