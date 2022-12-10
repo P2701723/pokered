afb8708ca462a30467b1452c9191e8438a87df07
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-10 11:30:01 -0500

    Update jigglypuff.asm

--- data/baseStats/jigglypuff.asm
+++ data/baseStats/jigglypuff.asm
@@ -1,10 +1,10 @@
 JigglypuffBaseStats: ; 38806 (e:4806)
 db DEX_JIGGLYPUFF ; pokedex id
-db 115 ; base hp
-db 45 ; base attack
-db 20 ; base defense
-db 20 ; base speed
-db 25 ; base special
+db 120 ; base hp      +5
+db 50 ; base attack   +5
+db 25 ; base defense  +5
+db 25 ; base speed    +5
+db 45 ; base special  +20
 db NORMAL ; species type 1
 db NORMAL ; species type 2
 db 170 ; catch rate
@@ -13,8 +13,8 @@ INCBIN "pic/bmon/jigglypuff.pic",0,1 ; 55, sprite dimensions
 dw JigglypuffPicFront
 dw JigglypuffPicBack
 ; attacks known at lvl 0
+db POUND
 db SING
-db 0
 db 0
 db 0
 db 4 ; growth rate
@@ -22,8 +22,8 @@ db 4 ; growth rate
 db %10110001
 db %00111111
 db %10101111
-db %11110001
-db %10100011
-db %00111000
+db %11111001 ; Dig
+db %10110011 ; Flamethrower
+db %00111010 ; Dream Eater
 db %01100011
 db 0 ; padding
