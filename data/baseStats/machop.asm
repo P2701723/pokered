b974ca8edad7331cf71dcfe7ccf5cb30cf79a7d2
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-10 14:32:43 -0500

    Update machop.asm

--- data/baseStats/machop.asm
+++ data/baseStats/machop.asm
@@ -3,7 +3,7 @@ db DEX_MACHOP ; pokedex id
 db 70 ; base hp
 db 80 ; base attack
 db 50 ; base defense
-db 35 ; base speed
+db 45 ; base speed    +10
 db 35 ; base special
 db FIGHTING ; species type 1
 db FIGHTING ; species type 2
@@ -23,7 +23,7 @@ db %10110001
 db %00000011
 db %00001111
 db %11001110
-db %10100110
+db %10110110 ; Flamethrower
 db %10001000
 db %00100010
 db 0 ; padding
