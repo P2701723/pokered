509ad6c18767a2f9947125ce24aae25e712f53cb
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-10 12:38:19 -0500

    Update venomoth.asm

--- data/baseStats/venomoth.asm
+++ data/baseStats/venomoth.asm
@@ -1,10 +1,10 @@
 VenomothBaseStats: ; 3891e (e:491e)
 db DEX_VENOMOTH ; pokedex id
-db $69 ; base hp
-db 65 ; base attack
-db 60 ; base defense
-db 90 ; base speed
-db $82 ; base special
+db 80 ; base hp		+10
+db 85 ; base attack	+20
+db 70 ; base defense	+10
+db 110 ; base speed	+20
+db 100 ; base special	+10
 db BUG ; species type 1
 db POISON ; species type 2
 db 75 ; catch rate
@@ -13,15 +13,10 @@ INCBIN "pic/bmon/venomoth.pic",0,1 ; 77, sprite dimensions
 dw VenomothPicFront
 dw VenomothPicBack
 ; attacks known at lvl 0
-db TACKLE
-db DISABLE
-IF DEF(_YELLOW)
-	db SUPERSONIC
-	db CONFUSION
-ELSE
-	db POISONPOWDER
-	db LEECH_LIFE
-ENDC
+db SUPERSONIC
+db CONFUSION
+db POISONPOWDER
+db LEECH_LIFE
 db 0 ; growth rate
 ; learnset
 db %00101010
@@ -30,5 +25,5 @@ db %00111000
 db %11110000
 db %01000011
 db %00101000
-db %00000010
+db %01000010 ; Flash
 db 0 ; padding
