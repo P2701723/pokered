9be9c4b916de362edf18acd39b0b4240f4ec1aa7
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-11 13:35:55 -0500

    Update digletts_cave_route_11_entrance.asm

--- text/maps/digletts_cave_route_11_entrance.asm
+++ text/maps/digletts_cave_route_11_entrance.asm
@@ -1,7 +1,8 @@
 _DiglettsCaveEntRoute11Text1:: ; 8c7f9 (23:47f9)
 	text "What a surprise!"
-	line "DIGLETT dug this"
-	cont "long tunnel!"
+	
+	para "DIGLETT dug this"
+	line "long tunnel!"
 
 	para "It goes right to"
 	line "PEWTER CITY!"
