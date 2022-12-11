328f60bc3e943fd1954063126addd27a377690b0
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-11 17:07:19 -0500

    Update slowpoke.asm

--- data/baseStats/slowpoke.asm
+++ data/baseStats/slowpoke.asm
@@ -13,8 +13,8 @@ INCBIN "pic/bmon/slowpoke.pic",0,1 ; 55, sprite dimensions
 dw SlowpokePicFront
 dw SlowpokePicBack
 ; attacks known at lvl 0
+db DISABLE
 db CONFUSION
-db 0
 db 0
 db 0
 db 0 ; growth rate
