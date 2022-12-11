2ad58a95a30d4e33c1909b47a633ae1f808698ea
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-11 18:48:36 -0500

    Update kingler.asm

--- data/baseStats/kingler.asm
+++ data/baseStats/kingler.asm
@@ -1,10 +1,10 @@
 KinglerBaseStats: ; 38e96 (e:4e96)
 db DEX_KINGLER ; pokedex id
-db $4b ; base hp
-db $91 ; base attack
-db $91 ; base defense
-db $5f ; base speed
-db $46 ; base special
+db 75 ; base hp       +20
+db 130 ; base attack
+db 115 ; base defense
+db 85 ; base speed    +10
+db 70 ; base special  +20
 db WATER ; species type 1
 db WATER ; species type 2
 db 60 ; catch rate
@@ -13,8 +13,8 @@ INCBIN "pic/bmon/kingler.pic",0,1 ; 77, sprite dimensions
 dw KinglerPicFront
 dw KinglerPicBack
 ; attacks known at lvl 0
-db BUBBLE
 db LEER
+db BUBBLE
 db VICEGRIP
 db 0
 db 0 ; growth rate
@@ -22,8 +22,8 @@ db 0 ; growth rate
 db %10100100
 db %01111111
 db %00001000
-db %11000000
+db %11001000 ; Dig
 db %00000010
-db %00001000
+db %10001000 ; Rock Slide
 db %00110110
 db 0 ; padding
