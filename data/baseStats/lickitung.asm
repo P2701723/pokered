00f37aa64571516a4c85b197b8d1ea75714fe42a
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-10 19:46:04 -0500

    Update lickitung.asm

--- data/baseStats/lickitung.asm
+++ data/baseStats/lickitung.asm
@@ -1,10 +1,10 @@
 LickitungBaseStats: ; 38f92 (e:4f92)
 db DEX_LICKITUNG ; pokedex id
-db 90 ; base hp
-db 55 ; base attack
-db 75 ; base defense
-db 30 ; base speed
-db 60 ; base special
+db 110 ; base hp      +20
+db 85 ; base attack   +30
+db 95 ; base defense  +20
+db 50 ; base speed    +20
+db 80 ; base special  +20
 db NORMAL ; species type 1
 db NORMAL ; species type 2
 db 45 ; catch rate
@@ -13,17 +13,17 @@ INCBIN "pic/bmon/lickitung.pic",0,1 ; 77, sprite dimensions
 dw LickitungPicFront
 dw LickitungPicBack
 ; attacks known at lvl 0
-db WRAP
 db SUPERSONIC
+db LICK
 db 0
 db 0
 db 0 ; growth rate
 ; learnset
 db %10110101
 db %01111111
-db %10001111
-db %11000111
-db %10100010
-db %00001000
+db %10101111 ; Solarbeam
+db %11001111 ; Dig
+db %10110010 ; Flamethrower
+db %10001010 ; Dream Eater, Rock Slide
 db %00110110
 db 0 ; padding
