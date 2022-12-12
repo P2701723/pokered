bf1e22dd326579de206284ef366df838bad9743b
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-11 19:54:25 -0500

    Update hitmonlee.asm

--- data/baseStats/hitmonlee.asm
+++ data/baseStats/hitmonlee.asm
@@ -2,9 +2,9 @@ HitmonleeBaseStats: ; 38f5a (e:4f5a)
 db DEX_HITMONLEE ; pokedex id
 db 50 ; base hp
 db 120 ; base attack
-db 53 ; base defense
-db 87 ; base speed
-db 35 ; base special
+db 78 ; base defense  +25
+db 97 ; base speed    +10
+db 110 ; base special +75
 db FIGHTING ; species type 1
 db FIGHTING ; species type 2
 db 45 ; catch rate
@@ -13,8 +13,8 @@ INCBIN "pic/bmon/hitmonlee.pic",0,1 ; 77, sprite dimensions
 dw HitmonleePicFront
 dw HitmonleePicBack
 ; attacks known at lvl 0
-db DOUBLE_KICK
 db MEDITATE
+db DOUBLE_KICK
 db 0
 db 0
 db 0 ; growth rate
@@ -22,8 +22,8 @@ db 0 ; growth rate
 db %10110001
 db %00000011
 db %00001111
-db %11000000
+db %11000110 ; Earthquake, Fissure
 db %11000110
-db %00001000
+db %10001000 ; Rock Slide
 db %00100010
 db 0 ; padding
