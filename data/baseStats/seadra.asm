445a24f948178d76cf3b8b1b16e20aaca1686725
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-11 21:40:21 -0500

    Update seadra.asm

--- data/baseStats/seadra.asm
+++ data/baseStats/seadra.asm
@@ -1,12 +1,12 @@
 SeadraBaseStats: ; 3908e (e:508e)
 db DEX_SEADRA ; pokedex id
-db 55 ; base hp
-db 65 ; base attack
+db 75 ; base hp       +20
+db 95 ; base attack   +30
 db 95 ; base defense
 db 85 ; base speed
 db 95 ; base special
 db WATER ; species type 1
-db WATER ; species type 2
+db DRAGON ; species type 2
 db 75 ; catch rate
 db 155 ; base exp yield
 INCBIN "pic/bmon/seadra.pic",0,1 ; 66, sprite dimensions
@@ -21,7 +21,7 @@ db 0 ; growth rate
 ; learnset
 db %00100000
 db %01111111
-db %00001000
+db %01001000 ; Dragon Rage
 db %11000000
 db %11000010
 db %00001000
