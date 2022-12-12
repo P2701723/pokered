4a15948cc3db2d19beeb4842c271ad331224d78b
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-11 19:47:09 -0500

    Update cubone.asm

--- data/baseStats/cubone.asm
+++ data/baseStats/cubone.asm
@@ -1,10 +1,10 @@
 CuboneBaseStats: ; 38f22 (e:4f22)
 db DEX_CUBONE ; pokedex id
 db 50 ; base hp
-db 50 ; base attack
+db 75 ; base attack   +25
 db 95 ; base defense
 db 35 ; base speed
-db 40 ; base special
+db 50 ; base special  +10
 db GROUND ; species type 1
 db GROUND ; species type 2
 db 190 ; catch rate
@@ -13,17 +13,17 @@ INCBIN "pic/bmon/cubone.pic",0,1 ; 55, sprite dimensions
 dw CubonePicFront
 dw CubonePicBack
 ; attacks known at lvl 0
-db BONE_CLUB
 db GROWL
-db 0
+db LEER
+db BONE_CLUB
 db 0
 db 0 ; growth rate
 ; learnset
-db %10110001
+db %10110101 ; Swords Dance
 db %00111111
 db %00001111
 db %11001110
-db %10100010
-db %00001000
+db %10110010 ; Flamethrower
+db %10001000 ; Rock Slide
 db %00100010
 db 0 ; padding
