ea676cdbaa5081f32d061bc028ba269e5c203a1d
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-11 19:54:21 -0500

    Update hitmonchan.asm

--- data/baseStats/hitmonchan.asm
+++ data/baseStats/hitmonchan.asm
@@ -2,9 +2,9 @@ HitmonchanBaseStats: ; 38f76 (e:4f76)
 db DEX_HITMONCHAN ; pokedex id
 db 50 ; base hp
 db 105 ; base attack
-db 79 ; base defense
-db 76 ; base speed
-db 35 ; base special
+db 104 ; base defense +25
+db 86 ; base speed    +10
+db 110 ; base special +75
 db FIGHTING ; species type 1
 db FIGHTING ; species type 2
 db 45 ; catch rate
@@ -13,8 +13,8 @@ INCBIN "pic/bmon/hitmonchan.pic",0,1 ; 66, sprite dimensions
 dw HitmonchanPicFront
 dw HitmonchanPicBack
 ; attacks known at lvl 0
-db COMET_PUNCH
 db AGILITY
+db COMET_PUNCH
 db 0
 db 0
 db 0 ; growth rate
@@ -22,8 +22,8 @@ db 0 ; growth rate
 db %10110001
 db %00000011
 db %00001111
-db %11000000
+db %11000110 ; Earthquake, Fissure
 db %11000110
-db %00001000
+db %10001000 ; Rock Slide
 db %00100010
 db 0 ; padding
