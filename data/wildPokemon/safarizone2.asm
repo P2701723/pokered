5c09fba6d01e435e352cd2a7507a54ee999c4f6a
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-17 14:31:39 -0500

    Update safarizone2.asm

--- data/wildPokemon/safarizone2.asm
+++ data/wildPokemon/safarizone2.asm
@@ -1,14 +1,14 @@
 ZoneMons2:
 	db $1E
-	db 40,NIDORINA
-	db 40,NIDORINO
-	db 37,EXEGGCUTE
-	db 37,RHYHORN
-	db 36,PARAS
-	db 40,VENOMOTH
-	db 35,KANGASKHAN
-	db 33,CUBONE
-	db 38,CHANSEY
-	db 39,TAUROS
+	db 40,NIDORINA		; 20% Nidorina
+	db 40,NIDORINO		; 20% Nidorino
+	db 37,EXEGGCUTE		; 15% Exeggcute
+	db 37,RHYHORN		; 10% Rhyhorn
+	db 36,PARAS		; 10% Paras
+	db 40,VENOMOTH		; 10% Venomoth
+	db 35,KANGASKHAN	; 5% Kangaskhan
+	db 33,CUBONE		; 5% Cubone
+	db 38,CHANSEY		; 4% Chansey
+	db 39,TAUROS		; 1% Tauros
 	db $00
 
