47dddd1420a72dbb84392e056c8d4a2a92bc6793
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-17 14:33:13 -0500

    Update safarizonecenter.asm

--- data/wildPokemon/safarizonecenter.asm
+++ data/wildPokemon/safarizonecenter.asm
@@ -1,14 +1,14 @@
 ZoneMonsCenter:
 	db $1e
-	db 40,NIDORINO
-	db 40,NIDORINA
-	db 37,EXEGGCUTE
-	db 37,RHYHORN
-	db 36,VENONAT
-	db 40,PARASECT
-	db 36,SCYTHER
-	db 36,PINSIR
-	db 36,TANGELA
-	db 36,CHANSEY
+	db 40,NIDORINO	; 20% Nidorino
+	db 40,NIDORINA	; 20% Nidorina
+	db 37,EXEGGCUTE	; 15% Exeggcute
+	db 37,RHYHORN	; 10% Rhyhorn
+	db 36,VENONAT	; 10% Venonat
+	db 40,PARASECT	; 10% Parasect
+	db 36,SCYTHER	; 5% Scyther
+	db 36,PINSIR	; 5% Pinsir
+	db 36,TANGELA	; 4% Tangela
+	db 36,CHANSEY	; 1% Chansey
 	db $00
 
