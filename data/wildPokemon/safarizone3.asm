38a69226127c154cdb66cb497ef99ce0714823fe
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-17 14:32:28 -0500

    Update safarizone3.asm

--- data/wildPokemon/safarizone3.asm
+++ data/wildPokemon/safarizone3.asm
@@ -1,14 +1,14 @@
 ZoneMons3:
 	db $1e
-	db 41,NIDORINO
-	db 36,EXEGGCUTE
-	db 37,NIDOQUEEN
-	db 38,DODUO
-	db 37,VENOMOTH
-	db 37,MAROWAK
-	db 35,TAUROS
-	db 39,PINSIR
-	db 39,KANGASKHAN
-	db 38,TANGELA
+	db 41,NIDORINO		; 20% Nidorino
+	db 36,EXEGGCUTE		; 20% Exeggcute
+	db 37,NIDOQUEEN		; 15% Nidoqueen
+	db 38,DODUO		; 10% Doduo
+	db 37,VENOMOTH		; 10% Venomoth
+	db 37,MAROWAK		; 10% Marowak
+	db 35,TAUROS		; 5% Tauros
+	db 39,PINSIR		; 5% Pinsir
+	db 39,KANGASKHAN	; 4% Kangaskhan
+	db 38,TANGELA		; 1% Tangela
 	db $00
 
