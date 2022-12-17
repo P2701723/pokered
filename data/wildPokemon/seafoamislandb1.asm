84f68fd5afd137f2b0fe4e8b0bc2002b220dde74
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-17 14:27:38 -0500

    Update seafoamislandb1.asm

--- data/wildPokemon/seafoamislandb1.asm
+++ data/wildPokemon/seafoamislandb1.asm
@@ -1,14 +1,14 @@
 IslandMonsB1:
 	db $0a
-	db 34,ZUBAT
-	db 40,KRABBY
-	db 39,SEEL
-	db 39,SLOWPOKE
-	db 40,SHELLDER
-	db 40,STARYU
-	db 40,HORSEA
-	db 39,PSYDUCK
-	db 38,GOLBAT
-	db 35,SQUIRTLE
+	db 34,ZUBAT	; 20% Zubat
+	db 40,KRABBY	; 20% Krabby
+	db 39,SEEL	; 15% Seel
+	db 39,SLOWPOKE	; 10% Slowpoke
+	db 40,SHELLDER	; 10% Shellder
+	db 40,STARYU	; 10% Staryu
+	db 40,HORSEA	; 5% Horsea
+	db 39,PSYDUCK	; 5% Psyduck
+	db 38,GOLBAT	; 4% Golbat
+	db 35,SQUIRTLE	; 1% Squirtle
 	db $00
 
