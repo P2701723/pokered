0db3681a45d614615210e3ae3a807417132a75e4
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-17 14:30:54 -0500

    Update safarizone1.asm

--- data/wildPokemon/safarizone1.asm
+++ data/wildPokemon/safarizone1.asm
@@ -1,14 +1,14 @@
 ZoneMons1:
 	db $1e
-	db 41,NIDORINA
-	db 36,EXEGGCUTE
-	db 37,NIDOKING
-	db 38,DODUO
-	db 37,PARASECT
-	db 37,MAROWAK
-	db 35,TAUROS
-	db 39,SCYTHER
-	db 35,CHANSEY
-	db 37,KANGASKHAN
+	db 41,NIDORINA		; 20% Nidorina
+	db 36,EXEGGCUTE		; 20% Exeggcute
+	db 37,NIDOKING		; 15% Nidoking
+	db 38,DODUO		; 10% Doduo
+	db 37,PARASECT		; 10% Parasect
+	db 37,MAROWAK		; 10% Marowak
+	db 35,TAUROS		; 5% Tauros
+	db 39,SCYTHER		; 5% Scyther
+	db 35,CHANSEY		; 4% Chansey
+	db 37,KANGASKHAN	; 1% Kangaskhan
 	db $00
 
