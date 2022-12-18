2d36d9f6708b62e1aefd6144aeb6e89d201877c7
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-18 09:13:27 -0500

    Update prizes.asm

--- data/prizes.asm
+++ data/prizes.asm
@@ -15,31 +15,35 @@ PrizeMenuMon1Entries: ; 52859 (14:6859)
 	db BULBASAUR
 	db CHARMANDER
 	db SQUIRTLE
+	db EEVEE
 	db "@"
 PrizeMenuMon1Cost: ; 5285d (14:685d)
 	db $10,$00
 	db $10,$00
 	db $10,$00
+	db $25,$00
 	db "@"
 
 PrizeMenuMon2Entries: ; 52864 (14:6864)
-	db EEVEE
+	db HITMONLEE
+	db HITMONCHAN
 	db DRATINI
 	db PORYGON
 	db "@"
 PrizeMenuMon2Cost: ; 52868 (14:6868)
-	db $25,$00
+	db $40,$00
+	db $40,$00
 	db $65,$00
 	db $99,$99
 	db "@"
 
 PrizeMenuTMsEntries: ; 5286f (14:686f)
-	db MOON_STONE
-	db RARE_CANDY
-	db PP_UP
+	db HELIX_FOSSIL
+	db DOME_FOSSIL
+	db MASTER_BALL
 	db "@"
 PrizeMenuTMsCost: ; 52873 (14:6873)
-	db $01,$05 ; 105 Coins
-	db $02,$40 ; 240 Coins
-	db $04,$90 ; 490 Coins
+	db $40,$00 ; 4000 Coins
+	db $40,$00 ; 4000 Coins
+	db $99,$99 ; 9999 Coins
 	db "@"
