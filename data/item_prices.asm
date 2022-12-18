4ddd9eefcae506ed7188f401595401bebfd4af7d
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-18 10:31:05 -0500

    Update item_prices.asm

--- data/item_prices.asm
+++ data/item_prices.asm
@@ -1,87 +1,87 @@
 ItemPrices: ; 4608 (1:4608)
-	db $00,$00,$00
-	db $00,$12,$00
-	db $00,$06,$00
-	db $00,$02,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$10,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$01,$00
-	db $00,$02,$50
-	db $00,$02,$50
-	db $00,$02,$50
-	db $00,$02,$00
-	db $00,$30,$00
-	db $00,$25,$00
-	db $00,$12,$00
-	db $00,$07,$00
-	db $00,$03,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$05,$50
-	db $00,$03,$50
-	db $00,$00,$00
-	db $00,$21,$00
-	db $00,$21,$00
-	db $00,$21,$00
-	db $00,$98,$00
-	db $00,$98,$00
-	db $00,$98,$00
-	db $00,$98,$00
-	db $00,$98,$00
-	db $00,$48,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$09,$50
-	db $00,$21,$00
-	db $00,$00,$00
-	db $01,$00,$00
-	db $00,$98,$00
-	db $00,$10,$00
-	db $00,$06,$00
-	db $00,$15,$00
-	db $00,$40,$00
-	db $00,$07,$00
-	db $00,$05,$00
-	db $00,$07,$00
-	db $00,$06,$50
-	db $00,$00,$10
-	db $00,$02,$00
-	db $00,$03,$00
-	db $00,$03,$50
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$05,$00
-	db $00,$05,$50
-	db $00,$03,$50
-	db $00,$03,$50
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
-	db $00,$00,$00
+	db $20,$00,$00 ; MASTER BALL
+	db $00,$12,$00 ; ULTRA BALL
+	db $00,$06,$00 ; GREAT BALL
+	db $00,$02,$00 ; POKé BALL
+	db $00,$00,$00 ; TOWN MAP
+	db $00,$00,$00 ; BICYCLE
+	db $00,$00,$00 ; SURFBOARD
+	db $00,$00,$00 ; SAFARI BALL
+	db $00,$00,$00 ; POKéDEX
+	db $00,$21,$00 ; MOON STONE
+	db $00,$01,$00 ; ANTIDOTE
+	db $00,$02,$50 ; BURN HEAL 
+	db $00,$02,$50 ; ICE HEAL
+	db $00,$02,$50 ; AWAKENING
+	db $00,$02,$00 ; PARLYZ HEAL
+	db $00,$30,$00 ; FULL RESTORE
+	db $00,$25,$00 ; MAX POTION
+	db $00,$12,$00 ; HYPER POTION
+	db $00,$07,$00 ; SUPER POTION
+	db $00,$03,$00 ; POTION
+	db $00,$00,$00 ; BOULDERBADGE
+	db $00,$00,$00 ; CASCADEBADGE
+	db $00,$00,$00 ; THUNDERBADGE
+	db $00,$00,$00 ; RAINBOWBADGE
+	db $00,$00,$00 ; MARSHBADGE
+	db $00,$00,$00 ; SOULBADGE
+	db $00,$00,$00 ; VOLCANOBADGE
+	db $00,$00,$00 ; EARTHBADGE
+	db $00,$05,$50 ; ESCAPE ROPE
+	db $00,$03,$50 ; REPEL
+	db $00,$00,$00 ; OLD AMBER
+	db $00,$21,$00 ; FIRE STONE
+	db $00,$21,$00 ; THUNDERSTONE
+	db $00,$21,$00 ; WATER STONE
+	db $00,$98,$00 ; HP UP
+	db $00,$98,$00 ; PROTEIN
+	db $00,$98,$00 ; IRON
+	db $00,$98,$00 ; CARBOS
+	db $00,$98,$00 ; CALCIUM
+	db $00,$48,$00 ; RARE CANDY
+	db $00,$00,$00 ; DOME FOSSIL
+	db $00,$00,$00 ; HELIX FOSSIL
+	db $00,$00,$00 ; SECRET KEY
+	db $00,$00,$00 ; unused
+	db $00,$00,$00 ; BIKE VOUCHER
+	db $00,$09,$50 ; X ACCURACY
+	db $00,$21,$00 ; LEAF STONE
+	db $00,$00,$00 ; CARD KEY
+	db $01,$00,$00 ; NUGGET
+	db $00,$00,$00 ; unused
+	db $00,$10,$00 ; POKé DOLL
+	db $00,$06,$00 ; FULL HEAL
+	db $00,$15,$00 ; REVIVE
+	db $00,$40,$00 ; MAX REVIVE
+	db $00,$07,$00 ; GUARD SPEC.
+	db $00,$05,$00 ; SUPER REPEL
+	db $00,$07,$00 ; MAX REPEL
+	db $00,$06,$50 ; DIRE HIT
+	db $00,$00,$10 ; COIN
+	db $00,$02,$00 ; FRESH WATER
+	db $00,$03,$00 ; SODA POP
+	db $00,$03,$50 ; LEMONADE
+	db $00,$00,$00 ; S.S.TICKET
+	db $00,$00,$00 ; GOLD TEETH
+	db $00,$05,$00 ; X ATTACK
+	db $00,$05,$50 ; X DEFEND
+	db $00,$03,$50 ; X SPEED
+	db $00,$03,$50 ; X SPECIAL
+	db $00,$00,$00 ; COIN CASE
+	db $00,$00,$00 ; OAK's PARCEL
+	db $00,$00,$00 ; ITEMFINDER
+	db $00,$00,$00 ; SILPH SCOPE
+	db $00,$00,$00 ; POKé FLUTE
+	db $00,$00,$00 ; LIFT KEY
+	db $00,$30,$00 ; EXP.ALL
+	db $00,$10,$00 ; OLD ROD
+	db $00,$20,$00 ; GOOD ROD
+	db $00,$00,$00 ; SUPER ROD
+	db $00,$98,$00 ; PP UP
+	db $00,$12,$00 ; ETHER
+	db $00,$20,$00 ; MAX ETHER
+	db $00,$30,$00 ; ELIXIR
+	db $00,$45,$00 ; MAX ELIXIR
 	db $00,$00,$00
 	db $00,$00,$00
 	db $00,$00,$00
