cb04cbcae840dd915c77863bfd20c6b3b4e5616b
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-18 09:13:22 -0500

    Update prize_mon_levels.asm

--- data/prize_mon_levels.asm
+++ data/prize_mon_levels.asm
@@ -2,7 +2,9 @@ PrizeMonLevelDictionary: ; 5298a (14:698a)
 	db BULBASAUR,20
 	db CHARMANDER,20
 	db SQUIRTLE,20
-
 	db EEVEE,30
+
+	db HITMONLEE,30
+	db HITMONCHAN,30
 	db DRATINI,30
 	db PORYGON,30
