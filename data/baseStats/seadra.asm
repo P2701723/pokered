9c2ec42150c64025458d70c4e2f5378c9c74db02
P2701723 <119725084+P2701723@users.noreply.github.com>
2022-12-20 20:28:58 -0500

    Update seadra.asm

--- data/baseStats/seadra.asm
+++ data/baseStats/seadra.asm
@@ -13,9 +13,9 @@ INCBIN "pic/bmon/seadra.pic",0,1 ; 66, sprite dimensions
 dw SeadraPicFront
 dw SeadraPicBack
 ; attacks known at lvl 0
+db LEER
 db WATER_GUN
 db SMOKESCREEN
-db 0
 db 0
 db 0 ; growth rate
 ; learnset
